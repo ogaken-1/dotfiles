3a3c79c86027a9bc0cc6de647b16a067538f89ae
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:12 +0900

    :+1: fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,37 @@
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_STATE_HOME "$HOME/.local/state"
+set -x XDG_CACHE_HOME "$HOME/.cache"
+
+fish_add_path "$HOME/bin"
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/go/bin"
+fish_add_path "$HOME/.cargo/bin"
+fish_add_path "$HOME/.rustup/toolchains/*/bin"
+fish_add_path "$HOME/.deno/bin"
+fish_add_path "$HOME/.ghcup/bin"
+fish_add_path "$HOME/.cabal/bin"
+
+set -x EDITOR 'nvim'
+set -x MANPAGER 'nvim +Man!'
+set -x SYSTEMD_EDITOR "$EDITOR"
+set -x LESSCHARSET 'utf-8'
+set -x LANG 'en_US.UTF-8'
+
+if status --is-interactive
+  abbr -a ls 'eza --icons auto'
+
+  # git
+  abbr -a gs 'git status --short --branch -uall'
+  abbr -a gsw 'git branch --format="%(refname:short)" | fzf | xargs git switch'
+  abbr -a ga 'git ls-files --exclude-standard --others --modified | fzf -m | xargs git add'
+  abbr -a gst 'git stash'
+  abbr -a gci 'git commit'
+  abbr -a gcif 'git commit --fixup='
+  abbr -a gcia 'git commit --amend'
+  abbr -a gp 'git push'
+  abbr -a gpu 'git push -u origin "$(git branch --show-current)"'
+  abbr -a gpf 'git push --force-if-includes --force-with-lease'
+
+  starship init fish | source
+end
