f1fe5febd0ad63097df31327bbec4c660f945e74
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:10 +0900

    feat(zsh): Improve tmux integration

--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -7,5 +7,23 @@ gitop () {
 }
 
 gcd () {
-  cd "$(ghq list --full-path | fzf)"
+  if [ -z "$1" ]; then
+    PROJECT_PATH="$(ghq list --full-path | fzf)"
+    if [ -z "${PROJECT_PATH}" ]; then
+      return
+    fi
+    SESSION_NAME="$(basename ${PROJECT_PATH} | sed -E 's/\./_/g')"
+    tmux new-session -d -s "${SESSION_NAME}" -c "${PROJECT_PATH}" || tmux a -t "${SESSION_NAME}"
+    if [ -n "${TMUX}" ]; then
+      tmux switch-client -t "${SESSION_NAME}"
+    else
+      tmux a -t "${SESSION_NAME}"
+    fi
+  else
+    cd "$1"
+  fi
+}
+
+attach () {
+  tmux a -t "$(tmux ls | fzf | sed -E 's/\s.\+///')"
 }
