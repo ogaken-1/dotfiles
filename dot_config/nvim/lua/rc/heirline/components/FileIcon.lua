7ce463368480e840f681e6e66eb7bec81e57c11c
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:15 +0900

    🐛(Neovim) bufferlineのftアイコンがカレントバッファを参照している

--- dot_config/nvim/lua/rc/heirline/components/FileIcon.lua
+++ dot_config/nvim/lua/rc/heirline/components/FileIcon.lua
@@ -1,6 +1,7 @@
 return {
   provider = function(self)
-    local filename = vim.api.nvim_buf_get_name(0)
+    self.bufnr = self.bufnr or vim.api.nvim_get_current_buf()
+    local filename = vim.api.nvim_buf_get_name(self.bufnr)
     local extension = vim.fn.fnamemodify(filename, ':e')
     local icon, icon_color = require('nvim-web-devicons').get_icon_color(filename, extension, { default = true })
     self.icon_color = icon_color
