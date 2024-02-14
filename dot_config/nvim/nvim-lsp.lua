6bf189c53c22a5826586216ff9730c3c5f092484
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:12 +0900

    🚀(Neovim) vim.lsp.lua を起動時に読み込まないようにする

--- dot_config/nvim/nvim-lsp.lua
+++ dot_config/nvim/nvim-lsp.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   group = 'VimRc',
+  desc = 'nvim-lsp 用のキーマッピングなどを設定する',
   callback = function(context)
     vim.keymap.set(
       'i',
@@ -74,66 +75,77 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
-vim.lsp.handlers['textDocument/signatureHelp'] = function(_, results, ctx, config)
-  local client = vim.lsp.get_client_by_id(ctx.client_id)
-  vim.lsp.handlers.signature_help(
-    _,
-    results,
-    ctx,
-    table.extend(config or {}, {
-      focusable = false,
-      border = 'rounded',
-      title = client.name,
-      noautocmd = true,
-    })
-  )
-end
+-- vim.lspの読み込みが発生して起動が遅くなるのでLspAttachまで遅延する
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = 'VimRc',
+  once = true,
+  desc = 'nvim-lspのhoverやdiagnosticの設定をする',
+  callback = function()
+    vim.lsp.handlers['textDocument/signatureHelp'] = function(_, results, ctx, config)
+      local client = vim.lsp.get_client_by_id(ctx.client_id)
+      vim.lsp.handlers.signature_help(
+        _,
+        results,
+        ctx,
+        table.extend(config or {}, {
+          focusable = false,
+          border = 'rounded',
+          title = client.name,
+          noautocmd = true,
+        })
+      )
+    end
 
-vim.lsp.handlers['textDocument/hover'] = function(_, results, ctx, config)
-  local client = vim.lsp.get_client_by_id(ctx.client_id)
-  vim.lsp.handlers.hover(
-    _,
-    results,
-    ctx,
-    table.extend(config or {}, {
-      focusable = false,
-      border = 'rounded',
-      title = client.name,
-      noautocmd = true,
-    })
-  )
-end
+    vim.lsp.handlers['textDocument/hover'] = function(_, results, ctx, config)
+      local client = vim.lsp.get_client_by_id(ctx.client_id)
+      vim.lsp.handlers.hover(
+        _,
+        results,
+        ctx,
+        table.extend(config or {}, {
+          focusable = false,
+          border = 'rounded',
+          title = client.name,
+          noautocmd = true,
+        })
+      )
+    end
 
-vim.diagnostic.config {
-  virtual_text = false,
-  float = {
-    border = 'single',
-    title = 'diagnostics',
-    header = {},
-    prefix = function(diag, _, _)
-      ---@type string
-      local highlight
-      if diag.severity == vim.diagnostic.severity.ERROR then
-        highlight = 'LspDiagnosticsError'
-      elseif diag.severity == vim.diagnostic.severity.WARN then
-        highlight = 'LspDiagnosticsWarning'
-      elseif diag.severity == vim.diagnostic.severity.INFO then
-        highlight = 'LspDiagnosticsInformation'
-      elseif diag.severity == vim.diagnostic.severity.HINT then
-        highlight = 'LspDiagnosticsHint'
-      end
+    vim.diagnostic.config {
+      virtual_text = false,
+      float = {
+        border = 'single',
+        title = 'diagnostics',
+        header = {},
+        prefix = function(diag, _, _)
+          ---@type string
+          local highlight
+          if diag.severity == vim.diagnostic.severity.ERROR then
+            highlight = 'LspDiagnosticsError'
+          elseif diag.severity == vim.diagnostic.severity.WARN then
+            highlight = 'LspDiagnosticsWarning'
+          elseif diag.severity == vim.diagnostic.severity.INFO then
+            highlight = 'LspDiagnosticsInformation'
+          elseif diag.severity == vim.diagnostic.severity.HINT then
+            highlight = 'LspDiagnosticsHint'
+          end
 
-      if diag.code ~= nil then
-        return ('%s[%s]: '):format(diag.source, diag.code), highlight
-      else
-        return ('%s: '):format(diag.source), highlight
-      end
-    end,
-    suffix = '',
-    -- source = 'always',
-  },
-}
+          if diag.code ~= nil then
+            return ('%s[%s]: '):format(diag.source, diag.code), highlight
+          else
+            return ('%s: '):format(diag.source), highlight
+          end
+        end,
+        suffix = '',
+        -- source = 'always',
+      },
+    }
+  end,
+})
 
+-- vim.lspの読み込みは発生しないのと、
+-- これが定義されてる前提のスクリプト(heirlineのコンポーネントとか)が
+-- エラーになるのですぐに実行する
 vim.fn.sign_define {
   {
     name = 'DiagnosticSignError',
