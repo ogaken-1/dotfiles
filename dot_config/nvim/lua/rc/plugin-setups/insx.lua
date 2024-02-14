7d557e9fb86685b820a97da3264f52e917d8d088
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:11 +0900

    refactor(insx): Use `insx.compose()`

--- dot_config/nvim/lua/rc/plugin-setups/insx.lua
+++ dot_config/nvim/lua/rc/plugin-setups/insx.lua
@@ -20,24 +20,22 @@ end
 
 local setup = function()
   local insx = require 'insx'
-  do -- handle <CR>
-    insx.add('<CR>', {
+
+  insx.add('<CR>', insx.compose {
+    {
       enabled = cmp_visible,
       action = function()
         local cmp = require 'cmp'
-        cmp.confirm {
-          select = true,
-          behavior = cmp.ConfirmBehavior.Replace,
-        }
+        cmp.confirm { select = true, behavior = cmp.ConfirmBehavior.Replace, }
       end,
-    })
-    insx.add('<CR>', {
+    },
+    {
       enabled = pum_visible,
       action = function()
         vim.fn['pum#map#confirm']()
       end,
-    })
-    insx.add('<CR>', {
+    },
+    {
       enabled = function()
         return 1 == vim.fn.pumvisible()
       end,
@@ -48,126 +46,123 @@ local setup = function()
           ctx.send '<C-y>'
         end
       end,
-    })
-    insx.add('<CR>', {
+    },
+    {
       priority = -1,
       enabled = function()
-        return 1 == vim.fn.exists '*lexima#expand'
+        return 1 ==
+            vim.fn.exists '*lexima#expand'
       end,
       action = function(ctx)
         ctx.send(vim.fn.keytrans(vim.fn['lexima#expand']('<CR>', 'i')))
       end,
-    })
-  end
+    },
+  })
 
-  do -- handle '<C-n>'
-    insx.add('<C-n>', {
+  insx.add('<C-n>', insx.compose {
+    {
       enabled = cmp_visible,
       action = function()
         local cmp = require 'cmp'
         cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
       end,
-    })
-    insx.add('<C-n>', {
+    },
+    {
       enabled = function()
         return 1 == vim.fn.pumvisible()
       end,
       action = function(ctx)
         ctx.send '<C-n>'
       end,
-    })
-    insx.add('<C-n>', {
+    },
+    {
       enabled = pum_visible,
       action = function()
         vim.fn['pum#map#insert_relative'](1)
       end,
-    })
-    insx.add('<C-n>', {
+    },
+    {
       priority = -1,
       action = function()
         vim.fn['ddc#map#manual_complete']()
       end
-    })
-  end
+    },
+  })
 
-  do -- handle '<C-p>'
-    insx.add('<C-p>', {
+  insx.add('<C-p>', insx.compose {
+    {
       enabled = cmp_visible,
       action = function()
         local cmp = require 'cmp'
         cmp.select_prev_item { behavior = cmp.SelectBehavior.Insert }
       end,
-    })
-    insx.add('<C-p>', {
+    },
+    {
       enabled = function()
         return 1 == vim.fn.pumvisible()
       end,
-      action = function(ctx)
-        ctx.send '<C-p>'
-      end,
-    })
-    insx.add('<C-p>', {
+      action = function(ctx) ctx.send '<C-p>' end,
+    },
+    {
       enabled = pum_visible,
-      action = function()
-        vim.fn['pum#map#insert_relative'](-1)
-      end,
-    })
-    insx.add('<C-p>', {
+      action = function() vim.fn['pum#map#insert_relative'](-1) end,
+    },
+    {
       priority = -1,
       action = function()
         vim.fn['ddc#map#manual_complete']()
       end
-    })
-  end
+    },
+  })
 
-  do -- handle '<C-g>'
-    insx.add('<C-g>', {
+  insx.add('<C-g>', insx.compose {
+    {
       enabled = cmp_visible,
       action = function()
         local cmp = require 'cmp'
         cmp.abort()
       end,
-    })
-    insx.add('<C-g>', {
+    },
+    {
       enabled = function()
         return 1 == vim.fn.pumvisible()
       end,
       action = function(ctx)
         ctx.send '<C-e>'
       end,
-    })
-    insx.add('<C-g>', {
+    },
+    {
       enabled = pum_visible,
       action = function()
         vim.fn['pum#map#cancel']()
       end,
-    })
-    insx.add('<C-g>', {
+    },
+    {
       priority = -1,
       action = function(ctx)
         ctx.send '<C-g>'
       end
-    })
-  end
+    },
+  })
 
-  do -- handle '<Tab>'
-    insx.add('<Tab>', {
+  insx.add('<Tab>', insx.compose {
+    {
       enabled = function()
         return vsnip_jumpable(1)
       end,
       action = function(ctx)
         ctx.send '<Plug>(vsnip-jump-next)'
       end,
-    })
-    insx.add('<Tab>', {
+    },
+    {
       enabled = function()
         return vim.fn.exists '*UltiSnips#CanJumpForwards' == 1 and vim.fn['UltiSnips#CanJumpForwards']() == 1
       end,
       action = function(ctx)
         ctx.send '<Plug>(ultisnips-jump-next)'
       end,
-    })
-    insx.add('<Tab>', {
+    },
+    {
       priority = -1,
       enabled = function()
         return 1 == vim.fn.exists '*lexima#expand'
@@ -175,27 +170,27 @@ local setup = function()
       action = function(ctx)
         ctx.send(vim.fn.keytrans(vim.fn['lexima#expand']('<TAB>', 'i')))
       end,
-    })
-  end
+    },
+  })
 
-  do -- handle '<S-Tab>'
-    insx.add('<S-Tab>', {
+  insx.add('<S-Tab>', insx.compose {
+    {
       enabled = function()
         return vsnip_jumpable(-1)
       end,
       action = function(ctx)
         ctx.send '<Plug>(vsnip-jump-next)'
       end,
-    })
-    insx.add('<S-Tab>', {
+    },
+    {
       enabled = function()
         return vim.fn.exists '*UltiSnips#CanJumpBackwards' == 1 and vim.fn['UltiSnips#CanJumpBackwards']() == 1
       end,
       action = function(ctx)
         ctx.send '<Plug>(ultisnips-jump-previous)'
       end,
-    })
-    insx.add('<S-Tab>', {
+    },
+    {
       priority = -1,
       enabled = function()
         return 1 == vim.fn.exists '*lexima#expand'
@@ -203,122 +198,122 @@ local setup = function()
       action = function(ctx)
         ctx.send(vim.fn.keytrans(vim.fn['lexima#expand']('<S-TAB>', 'i')))
       end,
-    })
-  end
+    },
+  })
 
   local function cmdline_add(keys, recipe)
     insx.add(keys, recipe, { mode = 'c' })
   end
 
-  do -- handle <Tab> in cmdline
-    cmdline_add('<Tab>', {
+  cmdline_add('<Tab>', insx.compose {
+    {
       enabled = cmp_visible,
       action = function()
         local cmp = require 'cmp'
         cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
       end,
-    })
-    cmdline_add('<Tab>', {
+    },
+    {
       enabled = pum_visible,
       action = function(ctx)
         vim.fn['pum#map#insert_relative'](1)
         ctx.send '<Cmd>redraw<CR>'
       end
-    })
-    cmdline_add('<Tab>', {
+    },
+    {
       enabled = wilder_visible,
       action = function()
         vim.fn['wilder#next']()
       end
-    })
-    cmdline_add('<Tab>', {
+    },
+    {
       priority = -1,
       action = function(ctx)
         ctx.send '<Tab>'
       end
-    })
-  end
+    }
+  })
 
-  do -- handle <S-Tab> in cmdline
-    cmdline_add('<S-Tab>', {
+  cmdline_add('<S-Tab>', insx.compose {
+    {
       enabled = cmp_visible,
       action = function()
         local cmp = require 'cmp'
         cmp.select_prev_item { behavior = cmp.SelectBehavior.Insert }
       end,
-    })
-    cmdline_add('<S-Tab>', {
+    },
+    {
       enabled = pum_visible,
       action = function(ctx)
         vim.fn['pum#map#insert_relative'](-1)
         ctx.send '<Cmd>redraw<CR>'
       end
-    })
-    cmdline_add('<S-Tab>', {
+    },
+    {
       enabled = wilder_visible,
       action = function()
         vim.fn['wilder#previous']()
       end
-    })
-    cmdline_add('<S-Tab>', {
+    },
+    {
       priority = -1,
       action = function(ctx)
         ctx.send '<S-Tab>'
       end
-    })
-  end
+    }
+  })
 
-  do -- handle <C-g> in cmdline
-    cmdline_add('<C-g>', {
+  cmdline_add('<C-g>', insx.compose {
+    {
       enabled = cmp_visible,
       action = function()
         local cmp = require 'cmp'
         cmp.close()
       end,
-    })
-    cmdline_add('<C-g>', {
+    },
+    {
       enabled = pum_visible,
       action = function()
         vim.fn['pum#map#cancel']()
       end
-    })
-    cmdline_add('<C-g>', {
+    },
+    {
       enabled = wilder_visible,
       action = function()
         vim.fn['wilder#reject_completion']()
       end
-    })
-    cmdline_add('<C-g>', {
+    },
+    {
       priority = -1,
       action = function(ctx)
         ctx.send '<C-e>'
       end
-    })
-  end
+    }
+  })
 
-  do -- handle <C-f> in cmdline
-    cmdline_add('<C-f>', {
+  cmdline_add('<C-f>', insx.compose {
+    {
       enabled = cmp_visible,
       action = function(ctx)
         local cmp = require 'cmp'
         cmp.close()
         ctx.send '<C-f>'
       end,
-    })
-    cmdline_add('<C-f>', {
+    },
+    {
       enabled = pum_visible,
       action = function(ctx)
         vim.fn['pum#map#cancel']()
         ctx.send '<C-f>'
       end
-    })
-    cmdline_add('<C-f>', {
+    },
+    {
       priority = -1,
       action = function(ctx)
         ctx.send '<C-f>'
       end
-    })
-  end
+    }
+  })
 end
 
 return {
