03f938ecc84c630eaabef66787d00f7494c3ec9e
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:12 +0900

    feat(ddc-dictionary): Use sqlite database

--- dot_config/nvim/lua/config/plugins/ddc/sources/dictionary.lua
+++ dot_config/nvim/lua/config/plugins/ddc/sources/dictionary.lua
@@ -1,3 +1,8 @@
+local databasePath = vim.fs.joinpath(vim.fn.stdpath 'cache', 'ddc', 'dictionary.db')
+if 0 == vim.fn.isdirectory(vim.fs.dirname(databasePath)) then
+  vim.fn.mkdir(vim.fs.dirname(databasePath))
+end
+
 return {
   name = 'dictionary',
   options = {
@@ -12,5 +17,6 @@ return {
     firstCaseInsensitive = true,
     -- yay -S wordnet-cli
     -- documentCommand = { 'wn', '{{word}}', '-over' },
+    databasePath = databasePath,
   },
 }
