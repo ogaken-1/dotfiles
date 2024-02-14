9ef6a8ce50ccdd1628244c5e76e5013c54f1ada5
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:13 +0900

    💩(Emacs) Update init.el

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -27,104 +27,113 @@
     (leaf-keywords-init)))
 ;; </leaf-install-code>
 
-(leaf *builtin
+(leaf cus-edit
+  :doc "tools for customizing Emacs and Lisp packages"
+  :tag "builtin" "faces" "help"
+  :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
+
+(leaf cus-start
+  :doc "define customization properties of builtins"
+  :tag "builtin" "internal"
+  :preface
+  (defun c/redraw-frame nil
+    (interactive)
+    (redraw-frame))
+  :bind (("M-ESC ESC" . c/redraw-frame))
+  :custom '((user-full-name . "Kento Ogata")
+	    (create-lockfiles . nil)
+	    (debug-on-error . t)
+	    (init-file-debug . t)
+	    (frame-resize-pixelwise . t)
+	    (enable-recursive-minibuffers . t)
+	    (history-length . 1000)
+	    (history-delete-duplicates . t)
+	    (scroll-preserve-screen-position . t)
+	    (scroll-conservatively . 100)
+	    (mouse-wheel-scroll-amount . '(1 ((control) . 5)))
+	    (ring-bell-function . 'ignore)
+	    (text-quoting-style . 'straight)
+	    (truncate-lines . t)
+	    (use-dialog-box . nil)
+	    (use-file-dialog . nil)
+	    (menu-bar-mode . nil)
+	    (tool-bar-mode . nil)
+	    (scroll-bar-mode . nil)
+	    (indent-tabs-mode . nil))
   :config
-  (leaf cus-edit
-    :doc "tools for customizing Emacs and Lisp packages"
-    :tag "builtin" "faces" "help"
-    :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
-  (leaf cus-start
-    :doc "define customization properties of builtins"
-    :tag "builtin" "internal"
-    :preface
-    (defun c/redraw-frame nil
-      (interactive)
-      (redraw-frame))
-    :bind (("M-ESC ESC" . c/redraw-frame))
-    :custom '((user-full-name . "Kento Ogata")
-	      (create-lockfiles . nil)
-	      (debug-on-error . t)
-	      (init-file-debug . t)
-	      (frame-resize-pixelwise . t)
-	      (enable-recursive-minibuffers . t)
-	      (history-length . 1000)
-	      (history-delete-duplicates . t)
-	      (scroll-preserve-screen-position . t)
-	      (scroll-conservatively . 100)
-	      (mouse-wheel-scroll-amount . '(1 ((control) . 5)))
-	      (ring-bell-function . 'ignore)
-	      (text-quoting-style . 'straight)
-	      (truncate-lines . t)
-	      (use-dialog-box . nil)
-	      (use-file-dialog . nil)
-	      (menu-bar-mode . nil)
-	      (tool-bar-mode . nil)
-	      (scroll-bar-mode . nil)
-	      (indent-tabs-mode . nil))
-    :config
-    (defalias 'yes-or-no-p 'y-or-n-p)
-    (keyboard-translate ?\C-h ?\C-?))
-  (leaf autorevert
-    :doc "revert buffers when files on disk change"
-    :tag "builtin"
-    :custom ((auto-revert-interval . 1))
-    :global-minor-mode global-auto-revert-mode)
-  (leaf cc-mode
-    :doc "major mode for editing C and similar languages"
-    :tag "builtin"
-    :defvar (c-basic-offset)
-    :bind (c-mode-base-map
-	   ("C-c c" . compile))
-    :mode-hook
-    (c-mode-hook . ((c-set-style "bsd")
-		    (setq c-basic-offset 4)))
-    (c++-mode-hook . ((c-set-style "bsd")
-		      (setq c-basic-offset 4))))
-  (leaf delsel
-    :doc "delete selection if you insert"
-    :tag "builtin"
-    :global-minor-mode delete-selection-mode)
-  (leaf paren
-    :doc "highlight matching paren"
-    :tag "builtin"
-    :custom ((show-paren-delay . 0.1))
-    :global-minor-mode show-paren-mode)
-  (leaf simple
-    :doc "basic editing commands for Emacs"
-    :tag "builtin" "internal"
-    :custom ((kill-ring-max . 100)
-	     (kill-read-only-ok . t)
-	     (kill-whole-line . t)
-	     (eval-expression-print-length . nil)
-	     (eval-expression-print-level . nil))
-    :global-minor-mode column-number-mode)
-  (leaf files
-    :doc "file input and output commands for Emacs"
-    :tag "builtin"
-    :custom `((auto-save-timeout . 15)
-	      (auto-save-interval . 60)
-	      (auto-save-file-name-transforms . '((".*" ,(locate-user-emacs-file "backup/") t)))
-	      (backup-directory-alist . '((".*" . ,(locate-user-emacs-file "backup"))
-					  (,tramp-file-name-regexp . nil)))
-	      (version-control . t)
-	      (delete-old-versions . t)))
-  (leaf startup
-    :doc "process Emacs shell arguments"
-    :tag "builtin" "internal"
-    :custom `((auto-save-list-file-prefix . ,(locate-user-emacs-file "backup/.saves-"))
-	      (inhibit-startup-message . t)
-	      (initial-scratch-message . "")))
-  (leaf time
-    :tag "builtin"
-    :global-minor-mode display-time-mode)
-  (leaf tool-bar
-    :tag "builtin"
-    :config
-    (tool-bar-mode -1))
-  (leaf mule-cmds
-    :tag "builtin"
-    :config
-    (prefer-coding-system 'utf-8)))
+  (defalias 'yes-or-no-p 'y-or-n-p)
+  (keyboard-translate ?\C-h ?\C-?))
+
+(leaf autorevert
+  :doc "revert buffers when files on disk change"
+  :tag "builtin"
+  :custom ((auto-revert-interval . 1))
+  :global-minor-mode global-auto-revert-mode)
+
+(leaf cc-mode
+  :doc "major mode for editing C and similar languages"
+  :tag "builtin"
+  :defvar (c-basic-offset)
+  :bind (c-mode-base-map
+	 ("C-c c" . compile))
+  :mode-hook
+  (c-mode-hook . ((c-set-style "bsd")
+		  (setq c-basic-offset 4)))
+  (c++-mode-hook . ((c-set-style "bsd")
+		    (setq c-basic-offset 4))))
+
+(leaf delsel
+  :doc "delete selection if you insert"
+  :tag "builtin"
+  :global-minor-mode delete-selection-mode)
+
+(leaf paren
+  :doc "highlight matching paren"
+  :tag "builtin"
+  :custom ((show-paren-delay . 0.1))
+  :global-minor-mode show-paren-mode)
+
+(leaf simple
+  :doc "basic editing commands for Emacs"
+  :tag "builtin" "internal"
+  :custom ((kill-ring-max . 100)
+	   (kill-read-only-ok . t)
+	   (kill-whole-line . t)
+	   (eval-expression-print-length . nil)
+	   (eval-expression-print-level . nil))
+  :global-minor-mode column-number-mode)
+
+(leaf files
+  :doc "file input and output commands for Emacs"
+  :tag "builtin"
+  :custom `((auto-save-timeout . 15)
+	    (auto-save-interval . 60)
+	    (auto-save-file-name-transforms . '((".*" ,(locate-user-emacs-file "backup/") t)))
+	    (backup-directory-alist . '((".*" . ,(locate-user-emacs-file "backup"))
+					(,tramp-file-name-regexp . nil)))
+	    (version-control . t)
+	    (delete-old-versions . t)))
+
+(leaf startup
+  :doc "process Emacs shell arguments"
+  :tag "builtin" "internal"
+  :custom `((auto-save-list-file-prefix . ,(locate-user-emacs-file "backup/.saves-"))
+	    (inhibit-startup-message . t)
+	    (initial-scratch-message . "")))
+
+(leaf time
+  :tag "builtin"
+  :global-minor-mode display-time-mode)
+
+(leaf tool-bar
+  :tag "builtin"
+  :config
+  (tool-bar-mode -1))
+
+(leaf mule-cmds
+  :tag "builtin"
+  :config
+  (prefer-coding-system 'utf-8))
 
 (leaf flycheck
   :doc "On-the-fly syntax checking"
@@ -163,7 +172,6 @@
   (add-hook 'org-mode-hook #'(lambda nil
 			       (setq-local company-begin-commands nil)))
   :global-minor-mode global-company-mode)
-
 
 (leaf ddskk
   :doc "Simple Kana to Kanji conversion program."
@@ -214,48 +222,44 @@
   :ensure t
   :global-minor-mode which-key-mode)
 
-(leaf *filetype
+(leaf csharp-mode :ensure t)
+
+(leaf yaml-mode :ensure t)
+
+(leaf markdown-mode :ensure t)
+
+(leaf dockerfile-mode :ensure t)
+
+(leaf csv-mode
+  :doc "Major mode for editing comma/char separated values"
+  :ensure t)
+
+(leaf org
+  :doc "Outline-based notes management and organizer"
+  :ensure t
+  :bind
+  (("C-c c" . org-capture)
+   ("C-c a" . org-agenda))
+  :custom
+  ((org-agenda-files . '(org-directory))
+   (org-startup-folded . nil))
   :config
-  (leaf csharp-mode :ensure t)
-
-  (leaf yaml-mode :ensure t)
-
-  (leaf markdown-mode :ensure t)
-
-  (leaf dockerfile-mode :ensure t)
-
-  (leaf csv-mode
-    :doc "Major mode for editing comma/char separated values"
-    :ensure t)
-
-  (leaf org
-    :doc "Outline-based notes management and organizer"
+  (leaf org-modern
     :ensure t
-    :bind (("C-c c" . org-capture)
-           ("C-c a" . org-agenda))
-    :custom ((org-agenda-files . '(org-directory))
-             (org-startup-folded . nil)
-             (org-capture-templates . '(("p" "電話番メモ" entry (file (concat org-directory "/incoming-call-history.org"))
-                                         "* %T \n- 会社: \n- 支店: \n- 部署: \n- 氏名: \n- 宛先: \n- 案件: \n- 要件: \n- 折り返し先: \n- 備考:")
-                                        ("t" "Todo" entry (file (concat org-directory "/notes.org"))
-                                         "* TODO \n Entry: %T"))))
     :init
-    (leaf org-journal
-      :doc "a simple org-mode based journaling mode"
-      :ensure t
-      :bind (("C-c j" . org-journal-new-entry))
-      :custom ('(org-journal-dir . ,(concat org-directory "/journal"))
-	       (org-journal-date-format . "%A, %d %B %Y")))
-    (leaf org-roam
-      :doc "A database abstraction layer for Org-mode"
-      :ensure t)
-    :config
-    (leaf org-modern
-      :ensure t
-      :init
-      (add-hook 'org-mode-hook #'org-modern-mode)
-      (add-hook 'org-agenda-finalize-hook #'org-modern-agenda))
-    (add-hook 'org-mode-hook #'org-indent-mode)))
+    (add-hook 'org-mode-hook #'org-modern-mode)
+    (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)))
+
+(leaf org-journal
+  :doc "a simple org-mode based journaling mode"
+  :ensure t
+  :bind (("C-c j" . org-journal-new-entry))
+  :custom ('(org-journal-dir . ,(concat org-directory "/journal"))
+	   (org-journal-date-format . "%A, %d %B %Y")))
+
+(leaf org-roam
+  :doc "A database abstraction layer for Org-mode"
+  :ensure t)
 
 (leaf editorconfig
   :ensure t
@@ -292,10 +296,6 @@
       :ensure t)
     :bind (("C-h b" . embark-bindings)))
   :global-minor-mode vertico-mode)
-
-(leaf evil
-  :ensure t
-  :bind ("C-x v" . evil-mode))
 
 (leaf magit
   :ensure t)
