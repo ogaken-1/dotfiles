2dca79ce3f59d6e308f62946546d2a215bfd3845
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:13 +0900

    feat(lsp): Add omnisharp's extended definition handler supports

--- dot_config/nvim/plugins.d/mason-lspconfig.lua
+++ dot_config/nvim/plugins.d/mason-lspconfig.lua
@@ -16,6 +16,9 @@ require('mason-lspconfig').setup_handlers {
   end,
   ['omnisharp'] = function()
     lspconfig.omnisharp.setup {
+      handlers = {
+        ['textDocument/definition'] = require('omnisharp_extended').handler,
+      },
       enable_editorconfig_support = true,
       enable_roslyn_analyzers = false,
       organize_imports_on_format = false,
