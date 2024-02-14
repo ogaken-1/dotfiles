0302ce4c69777c522427eca8140cacdb6675d41b
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:13 +0900

    👍(Neovim) statuslineにattachされているlsの名前を表示する

--- dot_config/nvim/lua/rc/heirline/components/LspActive.lua
+++ dot_config/nvim/lua/rc/heirline/components/LspActive.lua
@@ -3,21 +3,23 @@ local conditions = require 'heirline.conditions'
 return {
   condition = conditions.lsp_attached,
   update = { 'LspAttach', 'LspDetach', 'WinEnter' },
-  provider = ' [LSP]',
-  -- provider  = function(self)
-  --     local names = {}
-  --     for i, server in pairs(vim.lsp.buf_get_active_clients({ bufnr = 0 })) do
-  --         table.insert(names, server.name)
-  --     end
-  --     return " [" .. table.concat(names, " ") .. "]"
-  -- end,
+  provider = function(_)
+    local server_names = {}
+    for _, server in pairs(vim.lsp.get_active_clients { bufnr = 0 }) do
+      if server.name == 'null-ls' then
+        -- null-lsの場合は実際に使っているソースを羅列して表示する
+        local null_ls_sources = {}
+        for _, source in ipairs(require('null-ls.sources').get_available(vim.o.filetype)) do
+          table.insert(null_ls_sources, source.name)
+        end
+        table.insert(server_names, 'null-ls(' .. table.concat(null_ls_sources, ',') .. ')')
+      else
+        -- null-ls以外の場合はサーバー名をそのまま表示する
+        table.insert(server_names, server.name)
+      end
+    end
+    -- ' [lua_ls null-ls(stylua)]'
+    return ' [' .. table.concat(server_names, ' ') .. ']'
+  end,
   hl = { fg = 'green', bold = true },
-  on_click = {
-    name = 'heirline_LSP',
-    callback = function()
-      vim.schedule(function()
-        vim.cmd 'LspInfo'
-      end)
-    end,
-  },
 }
