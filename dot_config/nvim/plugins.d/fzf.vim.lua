e16a0805aa9549fc4ad3f4b23aabd4457811fa29
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:14 +0900

    🚀(Neovim) Use hooks_file

---
+++ dot_config/nvim/plugins.d/fzf.vim.lua
@@ -0,0 +1,42 @@
+-- lua_source {{{
+vim.api.nvim_create_user_command('FzfPlugins', function()
+  vim.fn['fzf#run'] {
+    source = vim.fn.map(vim.fn.values(vim.fn['dein#get']()), function(_, plugin)
+      return plugin.name
+    end),
+    sink = function(name)
+      require('fzf-lua').files {
+        cwd = vim.fn['dein#get'](name).path,
+      }
+    end,
+    window = {
+      width = 0.9,
+      height = 0.6,
+    },
+  }
+end, {})
+vim.api.nvim_create_user_command('FzfMrw', function()
+  vim.fn['fzf#run'] {
+    source = vim.fn['mr#mrw#list'](),
+    sink = 'edit',
+    window = {
+      width = 0.9,
+      height = 0.6,
+    },
+  }
+end, {})
+vim.api.nvim_create_user_command('GhqFind', function()
+  vim.fn['fzf#run'] {
+    source = vim.fn.systemlist { 'ghq', 'list', '--full-path' },
+    sink = function(path)
+      require('fzf-lua').files {
+        cwd = path,
+      }
+    end,
+    window = {
+      width = 0.9,
+      height = 0.6,
+    },
+  }
+end, {})
+-- }}}
