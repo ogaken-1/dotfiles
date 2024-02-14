368bf83ab12556ed2d298f276bab2a62d37b91ef
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:13 +0900

    feat(lsp): LSがインストールされていないときにsetupしない

--- nvim.d/lua/config/plugins/lspconfig.lua
+++ nvim.d/lua/config/plugins/lspconfig.lua
@@ -3,30 +3,34 @@ return {
   event = 'FileType',
   config = function()
     local lspconfig = require 'lspconfig'
-    lspconfig.lua_ls.setup {
-      settings = {
-        Lua = {
-          runtime = {
-            version = 'LuaJIT',
-          },
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME,
-              '${3rd}/luv/library',
-              '${3rd}/busted/library',
+    if 1 == vim.fn.executable 'lua-language-server' then
+      lspconfig.lua_ls.setup {
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT',
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                '${3rd}/luv/library',
+                '${3rd}/busted/library',
+              },
             },
           },
         },
-      },
-    }
-    lspconfig.omnisharp.setup {
-      enable_editorconfig_support = true,
-      enable_ms_build_load_projects_on_demand = false,
-      enable_roslyn_analyzers = true,
-      organize_imports_on_format = false,
-      enable_import_completion = false,
-      analyze_open_documents_only = true,
-    }
+      }
+    end
+    if 1 == vim.fn.executable 'omnisharp' then
+      lspconfig.omnisharp.setup {
+        enable_editorconfig_support = true,
+        enable_ms_build_load_projects_on_demand = false,
+        enable_roslyn_analyzers = true,
+        organize_imports_on_format = false,
+        enable_import_completion = false,
+        analyze_open_documents_only = true,
+      }
+    end
   end,
 }
