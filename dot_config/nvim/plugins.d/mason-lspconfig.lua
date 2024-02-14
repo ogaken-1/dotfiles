a9f47e364c3410162f3d646156eed8c246e31bc4
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:42:28 +0900

    refactor(lua_ls): `vim` global object hint is no longer unnecessary

--- dot_config/nvim/plugins.d/mason-lspconfig.lua
+++ dot_config/nvim/plugins.d/mason-lspconfig.lua
@@ -47,9 +47,6 @@ require('mason-lspconfig').setup_handlers {
           runtime = {
             version = 'LuaJIT',
           },
-          diagnostics = {
-            globals = { 'vim' },
-          },
           workspace = {
             checkThirdParty = false,
             library = vim.api.nvim_get_runtime_file('', true),
