b19952151b20980c11289bd36bcc8110aafca80a
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:10 +0900

    perf: Stop lazy loading of gin.vim

--- dot_config/nvim/denops/config/ddu/util.ts
+++ dot_config/nvim/denops/config/ddu/util.ts
@@ -2,13 +2,9 @@ import { Denops, fn } from "../../deps.ts";
 
 export function currentWorktree(denops: Denops): Promise<string | undefined>;
 export async function currentWorktree(denops: Denops) {
-  try {
-    return await denops.dispatch(
-      "gin",
-      "util:worktree",
-      await fn.bufname(denops),
-    );
-  } catch {
-    return undefined;
-  }
+  return await denops.dispatch(
+    "gin",
+    "util:worktree",
+    await fn.bufname(denops),
+  ).catch(() => undefined);
 }
