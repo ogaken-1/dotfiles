ad9777de1b837022a0570f7c598f68613af307fb
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:10 +0900

    chore: udd

--- dot_config/nvim/denops/deps.ts
+++ dot_config/nvim/denops/deps.ts
@@ -13,17 +13,17 @@ export { is } from "https://deno.land/x/unknownutil@v3.6.0/mod.ts";
 export {
   ActionFlags,
   BaseSource,
-} from "https://deno.land/x/ddu_vim@v3.5.1/types.ts";
+} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
 export type {
   ActionArguments,
   DduItem,
   DduOptions,
   Item,
-} from "https://deno.land/x/ddu_vim@v3.5.1/types.ts";
-export type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.5.1/base/source.ts";
+} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
+export type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.6.0/base/source.ts";
 
 export type { Params as FfParams } from "https://deno.land/x/ddu_ui_ff@v1.1.0/ff.ts";
-export type { ActionData as GitRepoActionData } from "https://raw.githubusercontent.com/KentoOgata/ddu-kind-git_repo/0.1.0/denops/@ddu-kinds/git_repo/types.ts";
+export type { ActionData as GitRepoActionData } from "https://raw.githubusercontent.com/KentoOgata/ddu-kind-git_repo/0.2.2/denops/@ddu-kinds/git_repo/types.ts";
 export type { ActionData as GitStatusActionData } from "https://raw.githubusercontent.com/kuuote/ddu-source-git_status/v1.0.0/denops/@ddu-kinds/git_status.ts";
 export { Source as GhqSource } from "https://raw.githubusercontent.com/4513ECHO/ddu-source-ghq/7df8ab95f648ee06f8e3f0e80ee639908dcd2a16/denops/@ddu-sources/ghq.ts";
 
