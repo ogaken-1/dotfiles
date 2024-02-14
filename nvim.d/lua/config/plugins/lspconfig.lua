53758db419eff98fb13a29a696f0d22539e6627b
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:13 +0900

    fix(lsp): Specify omnisharp cmd

--- nvim.d/lua/config/plugins/lspconfig.lua
+++ nvim.d/lua/config/plugins/lspconfig.lua
@@ -24,6 +24,7 @@ return {
     end
     if 1 == vim.fn.executable 'omnisharp' then
       lspconfig.omnisharp.setup {
+        cmd = { 'omnisharp' },
         enable_editorconfig_support = true,
         enable_ms_build_load_projects_on_demand = false,
         enable_roslyn_analyzers = true,
