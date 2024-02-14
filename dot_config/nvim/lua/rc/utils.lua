a9a07f9d32e00a592015030dfd7a0cdc6e089ef9
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:15 +0900

    🚿(Neovim) iif() を削除

--- dot_config/nvim/lua/rc/utils.lua
+++ dot_config/nvim/lua/rc/utils.lua
@@ -1,16 +1,16 @@
 return {
+  ---Pick a element of list
+  ---@generic T
+  ---@param list { [number]: T }
+  ---@return T
   pick = function(list)
     math.randomseed(os.time())
     return list[math.random(#list)]
   end,
+  ---Check string is nil or empty.
+  ---@param string any
+  ---@return boolean
   nilOrEmpty = function(string)
     return string == nil or string == ''
-  end,
-  iif = function(condition, ifTrue, ifFalse)
-    if condition then
-      return ifTrue
-    else
-      return ifFalse
-    end
   end,
 }
