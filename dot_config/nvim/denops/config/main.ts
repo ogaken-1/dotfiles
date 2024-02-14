2eda0dda7a8514ef1ed211471c24f858cf0cf04b
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:42:28 +0900

    refactor: Use std helper to define autocmd

--- dot_config/nvim/denops/config/main.ts
+++ dot_config/nvim/denops/config/main.ts
@@ -1,6 +1,7 @@
 import { join } from "https://deno.land/std@0.192.0/path/mod.ts";
 import { Denops } from "https://deno.land/x/denops_core@v5.0.0/mod.ts";
 import { e } from "https://deno.land/x/denops_std@v5.0.1/variable/environment.ts";
+import { group } from "https://deno.land/x/denops_std@v5.0.1/autocmd/group.ts";
 
 export async function main(denops: Denops) {
   denops.dispatcher = {
@@ -14,7 +15,14 @@ export async function main(denops: Denops) {
     },
   };
 
-  await denops.cmd(
-    `au VimRc InsertEnter * ++once call denops#notify('${denops.name}', 'ddc-setup', [])`,
-  );
+  await group(denops, "VimRc", (helper) => {
+    helper.define(
+      "InsertEnter",
+      "*",
+      `call denops#notify('${denops.name}', 'ddc-setup', [])`,
+      {
+        once: true,
+      },
+    );
+  });
 }
