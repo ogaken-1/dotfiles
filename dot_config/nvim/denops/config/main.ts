6d6bce2bfe5b12a100f9c876b282364315ec4def
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:12 +0900

    fixup! refactor(ddc): Setup code in lua

--- dot_config/nvim/denops/config/main.ts
+++ dot_config/nvim/denops/config/main.ts
@@ -1,8 +1,6 @@
 import { main as dduMain } from "./ddu/main.ts";
-import { main as ddcMain } from "./ddc/main.ts";
 import { Denops } from "../deps.ts";
 
 export async function main(denops: Denops) {
-  await ddcMain(denops);
   await dduMain(denops);
 }
