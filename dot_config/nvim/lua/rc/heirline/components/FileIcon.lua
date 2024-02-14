ad14816a90c5789188021a2ce53e6faf20377213
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:13 +0900

    👍(Neovim) ステータスラインのファイルアイコンの表示を良い感じに

--- dot_config/nvim/lua/rc/heirline/components/FileIcon.lua
+++ dot_config/nvim/lua/rc/heirline/components/FileIcon.lua
@@ -1,11 +1,10 @@
 return {
-  init = function(self)
-    local filename = self.filename
-    local extension = vim.fn.fnamemodify(filename, ':e')
-    self.icon, self.icon_color = require('nvim-web-devicons').get_icon_color(filename, extension, { default = true })
-  end,
   provider = function(self)
-    return self.icon and (self.icon .. ' ')
+    local filename = vim.api.nvim_buf_get_name(0)
+    local extension = vim.fn.fnamemodify(filename, ':e')
+    local icon, icon_color = require('nvim-web-devicons').get_icon_color(filename, extension, { default = true })
+    self.icon_color = icon_color
+    return icon and (icon .. ' ')
   end,
   hl = function(self)
     return { fg = self.icon_color }
--- dot_config/nvim/lua/rc/heirline/components/FileNameBlock.lua
+++ dot_config/nvim/lua/rc/heirline/components/FileNameBlock.lua
@@ -1,4 +1,3 @@
-local fileIcon = require 'rc.heirline.components.FileIcon'
 local fileName = require 'rc.heirline.components.FileName'
 local fileFlags = require 'rc.heirline.components.FileFlags'
 
@@ -6,7 +5,6 @@ return {
   init = function(self)
     self.filename = vim.api.nvim_buf_get_name(0)
   end,
-  fileIcon,
   fileName,
   unpack(fileFlags),
 }
