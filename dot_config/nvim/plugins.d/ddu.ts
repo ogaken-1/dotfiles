3a33bd7e502f5f9aefb3ae918d6e39867bd90d05
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:16 +0900

    💪 Use deno.land via import source instead of rawgithub

--- dot_config/nvim/plugins.d/ddu.ts
+++ dot_config/nvim/plugins.d/ddu.ts
@@ -1,38 +1,6 @@
 import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.2.7/base/config.ts";
-import {
-  ActionFlags,
-  BaseConfig,
-} from "https://deno.land/x/ddu_vim@v3.2.7/types.ts";
-import { Params as FfParams } from "https://raw.githubusercontent.com/Shougo/ddu-ui-ff/64167c0fd5fdcacf61bae8652a26f713e53d29a7/denops/%40ddu-uis/ff.ts";
-import * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
-import { Denops } from "https://deno.land/x/denops_core@v5.0.0/mod.ts";
-import { ActionData as FileKindActionData } from "https://deno.land/x/ddu_kind_file@v0.5.0/file.ts";
-
-const findRoot = async (denops: Denops, path: string) => {
-  const startDir = await fn.isdirectory(denops, path) === 1
-    ? path
-    : await fn.fnamemodify(denops, path, ":h");
-
-  for (
-    let dir = startDir;
-    dir !== "/";
-    dir = await fn.fnamemodify(denops, dir, ":h")
-  ) {
-    const files = await fn.readdir(
-      denops,
-      dir,
-      "{ fname -> fname ==# '.git' }",
-    );
-    if (files.length > 0) {
-      return dir;
-    }
-  }
-};
-
-const lcd = async (denops: Denops, dir: string) => {
-  await fn.execute(denops, ["lcd", dir]);
-  return Promise.resolve();
-};
+import { BaseConfig } from "https://deno.land/x/ddu_vim@v3.2.7/types.ts";
+import { Params as FfParams } from "https://deno.land/x/ddu_ui_ff@v1.0.2/ff.ts";
 
 export class Config extends BaseConfig {
   override config(args: ConfigArguments): Promise<void> {
