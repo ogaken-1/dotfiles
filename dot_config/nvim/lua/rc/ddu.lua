c69fdbe3b9e367399790bf6396b13a5e3ae26998
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:16 +0900

    👍(Neovim) `:lua require('rc.ddu').start 'source'` でdduを実行できるように

--- dot_config/nvim/lua/rc/ddu.lua
+++ dot_config/nvim/lua/rc/ddu.lua
@@ -78,253 +78,277 @@ local actionFlags = {
 ---@field actionName string
 ---@field func fun(args: any): DduActionFlags
 
+---@param config string|table
+---@return table
+local function normalizeConfig(config)
+  -- config is source name
+  if type(config) == 'string' then
+    return {
+      sources = {
+        {
+          name = config,
+        },
+      },
+    }
+    -- config is arguments of ddu#start
+  elseif type(config) == 'table' then
+    -- sourceの指定は ddu.start { 'file' } みたいにできるようにする
+    local normalizedConfig = {}
+    for k, v in pairs(config) do
+      if type(k) == 'number' then
+        normalizedConfig.sources = normalizedConfig.sources or {}
+        table.insert(normalizedConfig.sources, { name = v })
+      else
+        normalizedConfig[k] = v
+      end
+    end
+
+    return vim.tbl_deep_extend('keep', normalizedConfig, {
+      resume = false,
+      name = 'default',
+    })
+  end
+
+  return {}
+end
+
+---@param config string|table
+local function dduStart(config)
+  vim.fn['ddu#start'](normalizeConfig(config))
+end
+
 local ddu = {
   ---@param def DduCustomActionDefinition
   ---@return nil
-  customAction = function(def)
+  addCustomAction = function(def)
     vim.fn['ddu#custom#action'](def.type, def.name, def.actionName, def.func)
   end,
   ---@param config string|table
-  start = function(config)
-    -- config is source name
-    if type(config) == 'string' then
-      return function()
-        vim.fn['ddu#start'] {
-          sources = {
-            {
-              name = config,
-            },
-          },
-        }
-      end
-      -- config is arguments of ddu#start
-    elseif type(config) == 'table' then
-      -- sourceの指定は ddu.start { 'file' } みたいにできるようにする
-      local normalizedConfig = {}
-      for k, v in pairs(config) do
-        if type(k) == 'number' then
-          normalizedConfig.sources = normalizedConfig.sources or {}
-          table.insert(normalizedConfig.sources, { name = v })
-        else
-          normalizedConfig[k] = v
-        end
-      end
-
-      return function()
-        vim.fn['ddu#start'](vim.tbl_deep_extend('keep', normalizedConfig, {
-          resume = false,
-          name = 'default',
-        }))
-      end
+  getStartFunc = function(config)
+    return function()
+      dduStart(config)
     end
   end,
+  start = dduStart,
 }
 
-return {
-  setup = function()
-    ddu.customAction {
-      type = 'kind',
-      name = 'file',
-      actionName = 'openProject',
-      func = function(args)
-        vim.api.nvim_create_autocmd('FileType', {
-          pattern = 'ddu-ff',
-          once = true,
-          callback = function(ctx)
-            local wins = vim
-              .iter(vim.api.nvim_tabpage_list_wins(0))
-              :filter(function(win)
-                return vim.api.nvim_win_get_buf(win) == ctx.buf
-              end)
-              :totable()
-
-            if #wins == 0 then
-              return
-            end
-            local win = wins[1]
-
-            vim.wo[win].winbar = args.items[1].action.path
-          end,
-        })
-        vim.fn['ddu#start'] {
-          sources = {
-            {
-              name = 'file_external',
-            },
-          },
-          sourceParams = {
-            file_external = {
-              cmd = { 'git', 'ls-files', '-co', '--exclude-standard' },
-            },
-          },
-          sourceOptions = {
-            file_external = {
-              path = args.items[1].action.path,
-            },
-          },
-        }
+local function setup()
+  ddu.addCustomAction {
+    type = 'kind',
+    name = 'file',
+    actionName = 'openProject',
+    func = function(args)
+      if #args.items == 0 then
         return actionFlags.None
-      end,
-    }
+      end
+      local item = args.items[1]
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'ddu-ff',
+        once = true,
+        callback = function(ctx)
+          local wins = vim
+            .iter(vim.api.nvim_tabpage_list_wins(0))
+            :filter(function(win)
+              return vim.api.nvim_win_get_buf(win) == ctx.buf
+            end)
+            :totable()
 
-    vim.fn['ddu#custom#load_config'](vim.fs.joinpath(vim.env.DEIN_CONFIG_DIR, 'ddu.ts'))
+          if #wins == 0 then
+            return
+          end
+          local win = wins[1]
 
-    vim.api.nvim_create_autocmd('VimResized', {
-      group = 'VimRc',
-      callback = notifyWindowSizeToDdu,
-    })
-
-    notifyWindowSizeToDdu()
-
-    ---A wrapper of vim.keymap.set()
-    ---@param lhs string
-    ---@param rhs string|function
-    ---@param opts? table
-    local function nmap(lhs, rhs, opts)
-      vim.keymap.set('n', lhs, rhs, opts)
-    end
-
-    nmap('<Plug>(ddu-buffers)', ddu.start 'buffer')
-    nmap('<Plug>(ddu-files)', ddu.start 'file_external')
-    nmap('<Plug>(ddu-rg)', ddu.start 'rg')
-    nmap('<Plug>(ddu-lines)', ddu.start 'line')
-    nmap(
-      '<Plug>(ddu-mrw)',
-      ddu.start {
-        uiParams = {
-          ff = {
-            startFilter = false,
-          },
-        },
+          vim.wo[win].winbar = item.action.path
+        end,
+      })
+      vim.fn['ddu#start'] {
         sources = {
           {
-            name = 'mr',
-            params = {
-              kind = 'mrw',
-              current = true,
-            },
+            name = 'file_external',
           },
         },
-      }
-    )
-
-    nmap('<Plug>(ddu-help_tags)', ddu.start 'help')
-
-    nmap(
-      '<Plug>(ddu-lsp_implementations)',
-      ddu.start {
-        uiParams = {
-          ff = {
-            startAutoAction = true,
-            startFilter = false,
+        sourceParams = {
+          file_external = {
+            cmd = { 'git', 'ls-files', '-co', '--exclude-standard' },
           },
         },
-        sources = {
-          {
-            name = 'lsp_definition',
-            params = {
-              method = 'textDocument/implementation',
-            },
-          },
-        },
-      }
-    )
-
-    nmap(
-      '<Plug>(ddu-lsp_references)',
-      ddu.start {
-        'lsp_references',
-        uiParams = {
-          ff = {
-            startAutoAction = true,
-            startFilter = false,
-          },
-        },
-      }
-    )
-
-    nmap(
-      '<Plug>(ddu-resume)',
-      ddu.start {
-        resume = true,
-        name = 'default',
-        uiParams = {
-          ff = {
-            startFilter = false,
-          },
-        },
-      }
-    )
-
-    nmap(
-      '<Plug>(ddu-config_files)',
-      ddu.start {
-        'file_external',
         sourceOptions = {
           file_external = {
-            path = vim.fs.joinpath(vim.env.XDG_DATA_HOME, 'chezmoi'),
+            path = item.action.path,
           },
         },
       }
-    )
+      return actionFlags.None
+    end,
+  }
 
-    vim.api.nvim_create_user_command('DduPlugins', ddu.start 'dein', {})
-    vim.api.nvim_create_user_command('DduGhq', ddu.start 'ghq', {})
+  vim.fn['ddu#custom#load_config'](vim.fs.joinpath(vim.env.DEIN_CONFIG_DIR, 'ddu.ts'))
 
-    vim.api.nvim_create_autocmd('FileType', {
-      group = 'VimRc',
-      pattern = 'ddu-ff',
-      desc = 'Setup keymaps of ddu-ff',
-      callback = function(ctx)
-        vim.keymap.set_table {
-          mode = 'n',
-          opts = {
-            buffer = ctx.buf,
-          },
-          maps = {
-            { '<CR>', uiAction 'itemAction' },
-            { 'i', uiAction 'openFilterWindow' },
-            { 'q', uiAction 'quit' },
-            { 'd', itemAction 'delete' },
-            { '<SPACE>', uiAction 'toggleSelectItem', { nowait = true } },
-            { 'a', uiAction 'chooseAction' },
-            { '<C-Space>', uiAction 'toggleAllItems' },
-            { 'c', multiActions { 'toggleAllItems', { 'itemAction', { name = 'quickfix' } } } },
-            { 'p', uiAction 'toggleAutoAction' },
-          },
-        }
-      end,
-    })
+  vim.api.nvim_create_autocmd('VimResized', {
+    group = 'VimRc',
+    callback = notifyWindowSizeToDdu,
+  })
 
-    vim.api.nvim_create_autocmd('FileType', {
-      group = 'VimRc',
-      pattern = 'ddu-ff-filter',
-      desc = 'Setup keymaps of ddu-ff-filter',
-      callback = function(ctx)
-        vim.keymap.set('i', '<CR>', function()
-          vim.cmd.stopinsert()
-          vim.fn['ddu#ui#do_action'] 'closeFilterWindow'
-        end, { buffer = ctx.buf })
-        vim.keymap.set('n', '<CR>', uiAction 'closeFilterWindow', { buffer = ctx.buf })
-        vim.keymap.set('n', 'q', uiAction 'quit', { buffer = ctx.buf })
-      end,
-    })
+  notifyWindowSizeToDdu()
 
-    vim.keymap.set(
-      'n',
-      '<Plug>(lsp-codeAction)',
-      ddu.start {
-        'lsp_codeAction',
-        name = '_',
-        uiParams = {
-          ff = {
-            autoAction = {
-              name = 'preview',
-            },
-            startFilter = false,
+  ---A wrapper of vim.keymap.set()
+  ---@param lhs string
+  ---@param rhs string|function
+  ---@param opts? table
+  local function nmap(lhs, rhs, opts)
+    vim.keymap.set('n', lhs, rhs, opts)
+  end
+
+  nmap('<Plug>(ddu-buffers)', ddu.getStartFunc 'buffer')
+  nmap('<Plug>(ddu-files)', ddu.getStartFunc 'file_external')
+  nmap('<Plug>(ddu-rg)', ddu.getStartFunc 'rg')
+  nmap('<Plug>(ddu-lines)', ddu.getStartFunc 'line')
+  nmap(
+    '<Plug>(ddu-mrw)',
+    ddu.getStartFunc {
+      uiParams = {
+        ff = {
+          startFilter = false,
+        },
+      },
+      sources = {
+        {
+          name = 'mr',
+          params = {
+            kind = 'mrw',
+            current = true,
           },
         },
+      },
+    }
+  )
+
+  nmap('<Plug>(ddu-help_tags)', ddu.getStartFunc 'help')
+
+  nmap(
+    '<Plug>(ddu-lsp_implementations)',
+    ddu.getStartFunc {
+      uiParams = {
+        ff = {
+          startAutoAction = true,
+          startFilter = false,
+        },
+      },
+      sources = {
+        {
+          name = 'lsp_definition',
+          params = {
+            method = 'textDocument/implementation',
+          },
+        },
+      },
+    }
+  )
+
+  nmap(
+    '<Plug>(ddu-lsp_references)',
+    ddu.getStartFunc {
+      'lsp_references',
+      uiParams = {
+        ff = {
+          startAutoAction = true,
+          startFilter = false,
+        },
+      },
+    }
+  )
+
+  nmap(
+    '<Plug>(ddu-resume)',
+    ddu.getStartFunc {
+      resume = true,
+      name = 'default',
+      uiParams = {
+        ff = {
+          startFilter = false,
+        },
+      },
+    }
+  )
+
+  nmap(
+    '<Plug>(ddu-config_files)',
+    ddu.getStartFunc {
+      'file_external',
+      sourceOptions = {
+        file_external = {
+          path = vim.fs.joinpath(vim.env.XDG_DATA_HOME, 'chezmoi'),
+        },
+      },
+    }
+  )
+
+  vim.api.nvim_create_user_command('DduPlugins', ddu.getStartFunc 'dein', {})
+  vim.api.nvim_create_user_command('DduGhq', ddu.getStartFunc 'ghq', {})
+
+  vim.api.nvim_create_autocmd('FileType', {
+    group = 'VimRc',
+    pattern = 'ddu-ff',
+    desc = 'Setup keymaps of ddu-ff',
+    callback = function(ctx)
+      vim.keymap.set_table {
+        mode = 'n',
+        opts = {
+          buffer = ctx.buf,
+        },
+        maps = {
+          { '<CR>', uiAction 'itemAction' },
+          { 'i', uiAction 'openFilterWindow' },
+          { 'q', uiAction 'quit' },
+          { 'd', itemAction 'delete' },
+          { '<SPACE>', uiAction 'toggleSelectItem', { nowait = true } },
+          { 'a', uiAction 'chooseAction' },
+          { '<C-Space>', uiAction 'toggleAllItems' },
+          { 'c', multiActions { 'toggleAllItems', { 'itemAction', { name = 'quickfix' } } } },
+          { 'p', uiAction 'toggleAutoAction' },
+        },
       }
-    )
+    end,
+  })
+
+  vim.api.nvim_create_autocmd('FileType', {
+    group = 'VimRc',
+    pattern = 'ddu-ff-filter',
+    desc = 'Setup keymaps of ddu-ff-filter',
+    callback = function(ctx)
+      vim.keymap.set('i', '<CR>', function()
+        vim.cmd.stopinsert()
+        vim.fn['ddu#ui#do_action'] 'closeFilterWindow'
+      end, { buffer = ctx.buf })
+      vim.keymap.set('n', '<CR>', uiAction 'closeFilterWindow', { buffer = ctx.buf })
+      vim.keymap.set('n', 'q', uiAction 'quit', { buffer = ctx.buf })
+    end,
+  })
+
+  vim.keymap.set(
+    'n',
+    '<Plug>(lsp-codeAction)',
+    ddu.getStartFunc {
+      'lsp_codeAction',
+      name = '_',
+      uiParams = {
+        ff = {
+          autoAction = {
+            name = 'preview',
+          },
+          startFilter = false,
+        },
+      },
+    }
+  )
+end
+
+return setmetatable(ddu, {
+  __index = function(t, k)
+    if k == 'setup' then
+      return setup
+    else
+      return t[k]
+    end
   end,
-}
+})
