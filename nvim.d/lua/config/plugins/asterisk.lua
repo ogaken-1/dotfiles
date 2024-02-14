c73709e34ac0721c613e7856a96290e747740e8c
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:13 +0900

    feat(star): Don't jump, only search

---
+++ nvim.d/lua/config/plugins/asterisk.lua
@@ -0,0 +1,9 @@
+return {
+  'haya14busa/vim-asterisk',
+  keys = {
+    { '*', '<Plug>(asterisk-z*)', mode = { 'n', 'x' } },
+    { '#', '<Plug>(asterisk-z#)', mode = { 'n', 'x' } },
+    { 'g*', '<Plug>(asterisk-gz*)', mode = { 'n', 'x' } },
+    { 'g#', '<Plug>(asterisk-gz#)', mode = { 'n', 'x' } },
+  },
+}
