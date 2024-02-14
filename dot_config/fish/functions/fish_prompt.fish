8f3a9a9d64efd3533d80f9c7155716ab898a9a13
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:43:12 +0900

    feat(fish): Colorize prompt

--- dot_config/fish/functions/fish_prompt.fish
+++ dot_config/fish/functions/fish_prompt.fish
@@ -1,13 +1,14 @@
 function fish_prompt
   set components
 
-  set -a components "$USER"
+  set -a components (sgr color:yellow "$USER")
 
   # User can find that if he is in a git repository
   set -l git_repo $(git rev-parse --show-toplevel 2> /dev/null)
   if [ $status -eq 0 ]
     set -l git_branch $(git branch --show-current)
-    set -a components "($(basename "$git_repo"):$git_branch)"
+    set -l git_repo_name (sgr color:cyan (basename "$git_repo"))
+    set -a components "($git_repo_name:$git_branch)"
   end
 
   set -a components (path_shortn "$PWD")
---
+++ dot_config/fish/functions/sgr.fish
@@ -0,0 +1,20 @@
+function sgr -a command -a content
+  if [ -z "$command" ]
+    echo 'sgr: command is required.'
+    return 1
+  end
+
+  if [ "$command" = 'color:blue' ]
+    printf '\033[34m'
+  else if [ "$command" = 'color:cyan' ]
+    printf '\033[36m'
+  else if [ "$command" = 'color:yellow' ]
+    printf '\033[33m'
+  else
+    echo "sgr: command '$command' is not supported."
+    return 1
+  end
+
+  printf "$content"
+  printf '\033[0m'
+end
