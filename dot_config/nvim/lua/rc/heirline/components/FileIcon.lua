3cac740f057dfb5969925324176e02bb2bc22988
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:15 +0900

    👍(Neovim) `&buftype != ''` であるときはアイコンを表示しない

--- dot_config/nvim/lua/rc/heirline/components/FileIcon.lua
+++ dot_config/nvim/lua/rc/heirline/components/FileIcon.lua
@@ -1,11 +1,13 @@
 return {
   provider = function(self)
     self.bufnr = self.bufnr or vim.api.nvim_get_current_buf()
-    local filename = vim.api.nvim_buf_get_name(self.bufnr)
-    local extension = vim.fn.fnamemodify(filename, ':e')
-    local icon, icon_color = require('nvim-web-devicons').get_icon_color(filename, extension, { default = true })
-    self.icon_color = icon_color
-    return icon and (icon .. ' ')
+    if vim.api.nvim_get_option_value('buftype', { buf = self.bufnr }) == '' then
+      local filename = vim.api.nvim_buf_get_name(self.bufnr)
+      local extension = vim.fn.fnamemodify(filename, ':e')
+      local icon, icon_color = require('nvim-web-devicons').get_icon_color(filename, extension, { default = true })
+      self.icon_color = icon_color
+      return icon and (icon .. ' ')
+    end
   end,
   hl = function(self)
     return { fg = self.icon_color }
