0dfcf3430d118988599f53a2883f1bbc6468c6c4
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 00:40:14 +0900

    👍(Neovim) SKK-JISYOをdein.vimで取得する
    Updateもできて便利かも

--- dot_config/nvim/plugins.d/skkeleton.lua
+++ dot_config/nvim/plugins.d/skkeleton.lua
@@ -4,29 +4,16 @@ vim.api.nvim_create_autocmd('User', {
   once = true,
   pattern = 'DenopsPluginPost:skkeleton',
   callback = function()
-    ---@type string
-    local skkDataDir
-    if 1 == vim.fn.has 'win32' then
-      skkDataDir = vim.fs.joinpath(vim.env['APPDATA'], 'skk')
-    else
-      skkDataDir = vim.fs.joinpath(vim.env['XDG_DATA_HOME'], 'skk')
-    end
-    local dictPath = vim.fs.joinpath(skkDataDir, 'SKK-JISYO.L')
-
-    if 0 == vim.fn.filereadable(dictPath) then
-      local dictUrl = 'https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L'
-      os.execute('curl -fsSLo ' .. dictPath .. ' --create-dirs ' .. dictUrl)
-    end
-
     vim.fn['skkeleton_azik#setup'] {
       keys = {
         katakana = '[',
       },
     }
+    local skkDataDir = vim.fs.joinpath(vim.env['XDG_DATA_HOME'], 'skk')
     vim.fn['skkeleton#register_keymap']('henkan', 'p', 'purgeCandidate')
     vim.fn['skkeleton#config'] {
       eggLikeNewline = true,
-      globalJisyo = dictPath,
+      globalJisyo = vim.fn['GetDictPath'] 'L',
       userJisyo = vim.fs.joinpath(skkDataDir, 'user-jisyo'),
       completionRankFile = vim.fs.joinpath(skkDataDir, 'rank.json'),
       kanaTable = 'azik',
