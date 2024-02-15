eea169ed7a1d8c89bf7880a3b566838f22019974
Kento Ogata <k.ogata1013@gmail.com>
2024-02-15 22:20:19 +0900

    feat(Fern): Open cwd when cbuf is virtual buffer

--- nvim.d/lua/config/plugins/fern.lua
+++ nvim.d/lua/config/plugins/fern.lua
@@ -2,6 +2,16 @@ return {
   'lambdalisue/fern.vim',
   cmd = 'Fern',
   init = function()
-    vim.keymap.set('n', '<Space>e', '<Cmd>Fern %:h<CR>')
+    vim.keymap.set('n', '<Space>e', function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      ---@type string
+      local dir
+      if vim.api.nvim_get_option_value('buftype', { buf = bufnr }) == '' then
+        dir = '%:h'
+      else
+        dir = '.'
+      end
+      vim.cmd.Fern(dir)
+    end)
   end,
 }
