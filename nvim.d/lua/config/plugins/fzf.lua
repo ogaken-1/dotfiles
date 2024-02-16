fea005e2ad333d6205d5e65165b94e53c4cf4e77
Kento Ogata <k.ogata1013@gmail.com>
2024-02-16 11:17:41 +0900

    feat(fzf-lua): Update key mapping

--- nvim.d/lua/config/plugins/fzf.lua
+++ nvim.d/lua/config/plugins/fzf.lua
@@ -2,10 +2,12 @@ return {
   'ibhagwan/fzf-lua',
   cmd = 'FzfLua',
   init = function()
-    vim.keymap.set('n', ';f', '<Cmd>FzfLua files<CR>')
-    vim.keymap.set('n', ';h', '<Cmd>FzfLua help_tags<CR>')
-    vim.keymap.set('n', ';s', '<Cmd>FzfLua blines<CR>')
-    vim.keymap.set('n', ';b', '<Cmd>FzfLua buffers<CR>')
-    vim.keymap.set('n', ';z', '<Cmd>FzfLua oldfiles<CR>')
+    vim.keymap.set('n', 'U', '<C-r>')
+    vim.keymap.set('n', '<C-p>', '<Cmd>FzfLua files<CR>')
+    vim.keymap.set('n', '<C-h>', '<Cmd>FzfLua help_tags<CR>')
+    vim.keymap.set('n', '<C-s>', '<Cmd>FzfLua blines<CR>')
+    vim.keymap.set('n', '<C-n>', '<Cmd>FzfLua buffers<CR>')
+    vim.keymap.set('n', '<C-r>', '<Cmd>FzfLua oldfiles<CR>')
+  end,
   end,
 }
