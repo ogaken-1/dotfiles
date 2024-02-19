45e94a656c0af06036e1319e7c897f0ecd85a650
Kento Ogata <k.ogata1013@gmail.com>
2024-02-19 13:18:49 +0900

    feat(treesitter): Add unifieddiff parser
    - Add unifieddiff parser
    - Use diff parser in gin and gin-diff buffer

--- nvim.d/lua/config/plugins/treesitter.lua
+++ nvim.d/lua/config/plugins/treesitter.lua
@@ -1,11 +1,23 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  event = 'BufReadPost',
+  event = { 'BufReadPost', 'FileType' },
   config = function()
+    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+    parser_config.unifieddiff = {
+      install_info = {
+        url = 'https://github.com/monaqa/tree-sitter-unifieddiff.git',
+        files = { 'src/parser.c', 'src/scanner.c' },
+        filetype = 'diff',
+      },
+    }
+    vim.treesitter.language.register('unifieddiff', 'gin')
+    vim.treesitter.language.register('unifieddiff', 'gin-diff')
+
     vim.opt.rtp:prepend(vim.fs.joinpath(vim.fn.stdpath 'data', 'treesitter'))
     require('nvim-treesitter.configs').setup {
       ensure_installed = {
         'lua',
+        'unifieddiff',
       },
       highlight = {
         enable = true,
