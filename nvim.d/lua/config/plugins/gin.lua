ecf9fd4a0c909b182ce139c4aa5827a46950e57f
Kento Ogata <k.ogata1013@gmail.com>
2024-02-21 10:46:54 +0900

    feat(gin): Lcd for gin buffers

--- nvim.d/lua/config/plugins/gin.lua
+++ nvim.d/lua/config/plugins/gin.lua
@@ -22,7 +22,13 @@ return {
       group = gid,
       desc = 'Change cwd to git root of cbuf automatically.',
       callback = function(ctx)
-        if vim.fn.bufname(ctx.buf) ~= '' and vim.bo[ctx.buf].buftype == '' then
+        local function is_normal_buffer()
+          return vim.fn.bufname(ctx.buf) ~= '' and vim.bo[ctx.buf].buftype == ''
+        end
+        local function is_gin_buffer()
+          return vim.regex([[^gin.*:///]]):match_str(vim.fn.bufname(ctx.buf))
+        end
+        if is_normal_buffer() or is_gin_buffer() then
           vim.fn['denops#plugin#wait_async']('gin', vim.cmd.GinLcd)
         end
       end,
