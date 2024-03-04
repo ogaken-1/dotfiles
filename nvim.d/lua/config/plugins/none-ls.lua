a1f62bc55dc6be352e85fac5d0fa5292ea30ac76
Kento Ogata <k.ogata1013@gmail.com>
2024-03-04 14:11:23 +0900

    feat(shellcheck): Add shellcheck source of null-ls

--- nvim.d/lua/config/plugins/none-ls.lua
+++ nvim.d/lua/config/plugins/none-ls.lua
@@ -2,10 +2,13 @@ return {
   'nvimtools/none-ls.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    'gbprod/none-ls-shellcheck.nvim',
   },
   event = 'FileType',
   config = function()
     local null_ls = require 'null-ls'
+    null_ls.register(require 'none-ls-shellcheck.diagnostics')
+    null_ls.register(require 'none-ls-shellcheck.code_actions')
     null_ls.setup {
       sources = {
         null_ls.builtins.formatting.stylua,
