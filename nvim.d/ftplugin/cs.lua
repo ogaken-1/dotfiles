640ce7f1db5ec0d696a06672de9f35013cf04062
Kento Ogata <k.ogata1013@gmail.com>
2024-03-16 10:59:09 +0900

    feat: Csharpierをプラグインとして切り出した

--- nvim.d/ftplugin/cs.lua
+++ nvim.d/ftplugin/cs.lua
@@ -1,19 +1,6 @@
-if vim.b.did_my_ftplugin == 1 then
-  return
-end
-vim.b.did_my_ftplugin = 1
-
 vim.cmd.compiler 'dotnet_build'
 vim.bo.iskeyword = vim.bo.iskeyword .. ',@-@'
-vim.fn['denops#notify']('csharpier', 'startServer', {})
-local function format_cbuf()
-  vim.fn['denops#request']('csharpier', 'format', { vim.api.nvim_get_current_buf() })
-end
-vim.keymap.set('n', 'mf', format_cbuf)
-vim.api.nvim_create_autocmd('BufWritePre', {
-  buffer = vim.api.nvim_get_current_buf(),
-  callback = format_cbuf,
-})
+vim.keymap.set('n', 'mf', vim.fn['csharpier#formatfile'])
 vim.api.nvim_buf_create_user_command(vim.api.nvim_get_current_buf(), 'OpenRazor', function()
   local csharp_file_name = vim.fn.expand '%:p'
   local razor_file_name = vim.fn.fnamemodify(csharp_file_name, ':r:r') .. '.razor'
---
+++ nvim.d/lua/config/plugins/csharpier.lua
@@ -0,0 +1,7 @@
+return {
+  'ogaken-1/vim-csharpier',
+  dependencies = {
+    'vim-denops/denops.vim',
+  },
+  dev = true,
+}
