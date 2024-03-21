7541759f212d83b58ba09ea2da703c1142400031
Kento Ogata <k.ogata1013@gmail.com>
2024-03-21 17:25:36 +0900

    feat(Fern): 画面が十分に大きいときにドロワー表示を使う

--- nvim.d/lua/config/plugins/fern.lua
+++ nvim.d/lua/config/plugins/fern.lua
@@ -3,19 +3,26 @@ return {
   cmd = 'Fern',
   init = function()
     vim.keymap.set('n', '<Space>e', function()
-      local bufnr = vim.api.nvim_get_current_buf()
-      ---@type string
-      local dir
-      if vim.api.nvim_get_option_value('buftype', { buf = bufnr }) == '' then
-        dir = '%:h'
+      if vim.go.columns < 200 then
+        local bufnr = vim.api.nvim_get_current_buf()
+        ---@type string
+        local dir
+        if vim.api.nvim_get_option_value('buftype', { buf = bufnr }) == '' then
+          dir = '%:h'
+        else
+          dir = '.'
+        end
+        vim.cmd.Fern(dir)
+        return
       else
-        dir = '.'
+        vim.cmd.Fern { '.', '-drawer', '-reveal=%' }
+        return
       end
-      vim.cmd.Fern(dir)
     end)
   end,
   config = function()
     vim.g['fern#hide_cursor'] = 1
     vim.g['fern#default_hidden'] = 1
+    vim.g['fern#drawer_width'] = 60
   end,
 }
