3cd742881f4760c335f30dac1729cc2ed6e4b972
Kento Ogata <k.ogata1013@gmail.com>
2024-03-25 20:01:46 +0900

    fix(cmp): skkeletonの入力中にbuffer sourceの補完が出て鬱陶しい

--- nvim.d/lua/config/plugins/cmp.lua
+++ nvim.d/lua/config/plugins/cmp.lua
@@ -1,3 +1,23 @@
+local function setup_normal_sources()
+  local cmp = require 'cmp'
+  cmp.setup {
+    sources = cmp.config.sources({
+      { name = 'denippet' },
+      { name = 'nvim_lsp' },
+    }, {
+      { name = 'buffer' },
+    }),
+  }
+end
+local function setup_skk_source()
+  local cmp = require 'cmp'
+  cmp.setup {
+    sources = cmp.config.sources {
+      { name = 'skkeleton' },
+    },
+  }
+end
+
 return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
@@ -23,17 +43,21 @@ return {
           border = 'single',
         },
       },
-      sources = cmp.config.sources({
-        { name = 'skkeleton' },
-      }, {
-        { name = 'denippet' },
-        { name = 'nvim_lsp' },
-      }, {
-        { name = 'buffer' },
-      }),
       experimental = {
         ghost_text = true,
       },
     }
+    setup_normal_sources()
+    local gid = vim.api.nvim_create_augroup('config-cmp', { clear = true })
+    vim.api.nvim_create_autocmd('User', {
+      group = gid,
+      pattern = 'skkeleton-enable-post',
+      callback = setup_skk_source,
+    })
+    vim.api.nvim_create_autocmd('User', {
+      group = gid,
+      pattern = 'skkeleton-disable-post',
+      callback = setup_normal_sources,
+    })
   end,
 }
