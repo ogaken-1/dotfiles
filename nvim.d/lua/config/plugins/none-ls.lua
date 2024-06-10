746ae2f76c8e233ad374053a39600fa731b9e7a7
Kento Ogata <k.ogata1013@gmail.com>
2024-06-10 19:49:50 +0900

    feat(null_ls): Add prettierd source

--- nvim.d/lua/config/plugins/none-ls.lua
+++ nvim.d/lua/config/plugins/none-ls.lua
@@ -9,10 +9,17 @@ return {
     local null_ls = require 'null-ls'
     null_ls.register(require 'none-ls-shellcheck.diagnostics')
     null_ls.register(require 'none-ls-shellcheck.code_actions')
-    null_ls.setup {
-      sources = {
-        null_ls.builtins.formatting.stylua,
-      },
-    }
+    local sources = {}
+    local function add(source)
+      table.insert(sources, source)
+    end
+    local function add_if_exists(cmd, source)
+      if vim.fn.executable(cmd) == 1 then
+        add(source)
+      end
+    end
+    add_if_exists('prettierd', null_ls.builtins.formatting.prettierd)
+    add_if_exists('stylua', null_ls.builtins.formatting.stylua)
+    null_ls.setup { sources = sources }
   end,
 }
