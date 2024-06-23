c23ed9296c89144ccc6693b27bc886a8002f15be
Kento Ogata <k.ogata1013@gmail.com>
2024-06-24 04:44:32 +0900

    feat: Replace indent_blankline with hlchunk

---
+++ nvim.d/lua/config/plugins/hlchunk.lua
@@ -0,0 +1,12 @@
+return {
+  'shellRaining/hlchunk.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  opts = {
+    chunk = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+  },
+}
--- nvim.d/lua/config/plugins/indent_blankline.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'lukas-reineke/indent-blankline.nvim',
-  event = 'BufReadPost',
-  config = function()
-    require('ibl').setup()
-  end,
-}
