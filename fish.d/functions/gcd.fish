3d261aba6abab64684393eadd3aee47977cd948b
Kento Ogata <k.ogata1013@gmail.com>
2024-07-03 09:57:25 +0900

    fix(fish): `gcd` doesn't works when `ghq` does not exist

--- fish.d/functions/gcd.fish
+++ fish.d/functions/gcd.fish
@@ -1,8 +1,8 @@
 function gcd -d 'Select git project and start a tmux session.'
-  set -l repo (ghq list --full-path | fzf --prompt='Project >' --preview 'bat {}/README.*' --ansi)
+  set -l repo (__find_repos | fzf --prompt='Project >' --preview 'bat {}/README.md' --ansi)
 
   if [ -z "$repo" ]
-    echoerr 'ghq-cd: No projects selected.'
+    echoerr 'gcd: No projects selected.'
     return 1
   end
 
@@ -14,5 +14,15 @@ function gcd -d 'Select git project and start a tmux session.'
     tmux switch-client -t "$session_name"
   else
     tmux attach-session -t "$session_name"
+  end
+end
+
+function __find_repos
+  if which ghq > /dev/null 2> /dev/null
+    ghq list --full-path
+    return
+  end
+  find ~/repos -name '.git' -type d | while read -l git_dir
+    dirname $git_dir
   end
 end
