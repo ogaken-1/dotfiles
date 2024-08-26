a74cf92d96647ba256733db5d8c9afdb17c0bd5d
Kento Ogata <k.ogata1013@gmail.com>
2024-08-26 13:20:43 +0900

    feat(nvim): Don't invoke snipewin when windows.count <= 2

--- nvim.d/lua/config/plugins/snipewin.lua
+++ nvim.d/lua/config/plugins/snipewin.lua
@@ -1,6 +1,12 @@
 return {
   '4513ECHO/vim-snipewin',
-  keys = {
-    { '<C-w><C-w>', '<Plug>(snipewin)' },
-  },
+  keys = { '<Plug>(snipewin)' },
+  init = function()
+    vim.keymap.set('n', '<C-w><C-w>', function()
+      if #vim.api.nvim_tabpage_list_wins(0) <= 2 then
+        return '<C-w><C-w>'
+      end
+      return '<Plug>(snipewin)'
+    end, { expr = true })
+  end,
 }
