4f1d2788e75cfabd42b344979da711a27ed505ba
Kento Ogata <k.ogata1013@gmail.com>
2024-09-06 17:50:13 +0900

    fix(switch-window): SnipeWin invoked even count(focusable wins) == 2

--- nvim.d/lua/config/plugins/snipewin.lua
+++ nvim.d/lua/config/plugins/snipewin.lua
@@ -3,7 +3,13 @@ return {
   keys = { '<Plug>(snipewin)' },
   init = function()
     vim.keymap.set('n', '<C-w><C-w>', function()
-      if #vim.api.nvim_tabpage_list_wins(0) <= 2 then
+      local focusable_wins = vim
+        .iter(vim.api.nvim_tabpage_list_wins(0))
+        :filter(function(winid)
+          return vim.api.nvim_win_get_config(winid).focusable
+        end)
+        :totable()
+      if #focusable_wins <= 2 then
         return '<C-w><C-w>'
       end
       return '<Plug>(snipewin)'
