eba9c09ba221ac0fc8cfbba7a81e6ad18e0dbb5b
Kento Ogata <k.ogata1013@gmail.com>
2024-10-01 00:38:59 +0000

    refactor: Write neovim dependencies into programs/neovim

--- home.nix
+++ home.nix
@@ -11,32 +11,18 @@ in
     packages = with pkgs; [
       azure-cli
       bc
-      deno
       fd
       gcc14
       ghq
       gnumake
       gnupg
-      gopls
       kubectl
-      lua-language-server
-      nil
-      nixfmt-rfc-style
       postgresql_16
       ripgrep
-      shellcheck
-      skk-dicts
-      stylua
       tailscale
-      trash-cli
       vim
-      vscode-langservers-extracted
       wget
-      yaml-language-server
     ];
-    sessionVariables = {
-      SKK_DICT_DIR = "${pkgs.skk-dicts}/share";
-    };
   };
   xdg.enable = true;
   imports = programs;
--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -1,8 +1,27 @@
+{ pkgs, ... }:
 {
-  home.file = {
-    ".config/nvim/" = {
-      source = ./config.d;
-      recursive = true;
+  home = {
+    file = {
+      ".config/nvim/" = {
+        source = ./config.d;
+        recursive = true;
+      };
+    };
+    packages = with pkgs; [
+      deno
+      gopls
+      lua-language-server
+      nil
+      nixfmt-rfc-style
+      shellcheck
+      skk-dicts
+      stylua
+      trash-cli
+      vscode-langservers-extracted
+      yaml-language-server
+    ];
+    sessionVariables = {
+      SKK_DICT_DIR = "${pkgs.skk-dicts}/share";
     };
   };
   programs.neovim = {
