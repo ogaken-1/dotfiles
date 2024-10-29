dc7308a6c3c4225bf09388af068d31923b617e45
Kento Ogata <k.ogata1013@gmail.com>
2024-10-29 10:43:46 +0900

    feat(nvim): Use nightly version

--- flake.nix
+++ flake.nix
@@ -11,11 +11,32 @@
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-compat.follows = "flake-compat";
     };
     vscode-server = {
       url = "github:nix-community/nixos-vscode-server";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
+    };
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.git-hooks.follows = "git-hooks";
+      inputs.hercules-ci-effects.follows = "hercules-ci-effects";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-compat.follows = "flake-compat";
+    };
+    hercules-ci-effects = {
+      url = "github:hercules-ci/hercules-ci-effects";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
     };
   };
   outputs =
@@ -25,7 +46,7 @@
       vscode-server,
       nixos-wsl,
       ...
-    }:
+    }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -33,7 +54,14 @@
     {
       homeConfigurations.ogaken = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home.nix ];
+        modules = [
+          {
+            programs.neovim = {
+              package = inputs.neovim-nightly-overlay.packages.${system}.default;
+            };
+          }
+          ./home.nix
+        ];
       };
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = system;
