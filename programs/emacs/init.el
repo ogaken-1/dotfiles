a445d71aa4586ed2039e6a0909d5de2a828ff18a
Kento Ogata <k.ogata1013@gmail.com>
2024-12-09 13:16:15 +0900

    refactor(init.el): Remove unnecessary (requrie 'leaf)
    'leafはautoloadとして登録されているのでパッケージを明示的にrequireする
    必要がない

--- programs/emacs/init.el
+++ programs/emacs/init.el
@@ -13,7 +13,6 @@
                        ("melpa" . "https://melpa.org/packages/")
                        ("org" . "https://orgmode.org/elpa/")))
   (package-initialize)
-  (require 'leaf)
   (leaf leaf-keywords
     :init
     (leaf blackout)
