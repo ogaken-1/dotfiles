403104436d418f90c30016c8656140df91f8dae6
Kento Ogata <k.ogata1013@gmail.com>
2025-01-17 10:16:38 +0900

    fix(fish:cd): dirp doesn't work with multiple shell sessions
    ファイルにcdhistを書き込むと複数のシェルセッションがあるときに
    正しく動作しなくなるので、fishのグローバル変数に保存するように変更した。

--- programs/fish/functions/cd.fish
+++ programs/fish/functions/cd.fish
@@ -1,43 +1,33 @@
 function cd
+  set -f cdir "$PWD"
+  # Invoke fzf to choice target directory
+  # If this command called with no arguments
   if [ (count $argv) -eq 0 ]
-    set -l dir (fd . -t directory | fzf)
+    set -f dir (fd . -t directory | fzf)
     if [ -n "$dir" ]
       builtin cd "$dir"
-      __dirhistadd
+      __set_dirprev "$cdir"
     end
     return
   end
   # Use `cd -` for moving last directory
   if [ "$argv" = '-' ]
-    __dirp
+    if [ -d "$dirprev" ]
+      builtin cd "$dirprev"
+      __set_dirprev "$cdir"
+    else
+      echo "cd: directory $dirprev does not exist."
+    end
     return
   end
   builtin cd $argv
-  __dirhistadd
+  __set_dirprev "$cdir"
 end
 
 complete --command cd --arguments '(__fish_complete_directories)'
 
-function __dirhistadd
-  if [ -z "$XDG_STATE_HOME" ]
-    return 1
+function __set_dirprev -a dir
+  if [ "$dir" != "$dirprev"  ]
+    set -g dirprev "$dir"
   end
-  set -l dir "$XDG_STATE_HOME/fish"
-  [ -d "$dir" ] || mkdir -p "$dir"
-  set -l dirh "$dir/dirh"
-  set -l last (tail --lines 1 "$dirh")
-  if [ "$PWD" != "$last" ]
-    echo "$PWD" >> "$dirh"
-  end
-end
-
-function __dirp
-  if [ -z "$XDG_STATE_HOME" ]
-    return 1
-  end
-  set -l dir "$XDG_STATE_HOME/fish"
-  [ -d "$dir" ] || mkdir -p "$dir"
-  set -l dirh "$dir/dirh"
-  set -l prev (tail --lines 2 "$dirh" | head --lines 1)
-  [ -d "$dir" ] && cd "$prev"
 end
