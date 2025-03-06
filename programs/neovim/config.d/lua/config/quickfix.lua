33d9f56fd21fe8853a0f123fd55c6e17cfb9b5f5
Kento Ogata <k.ogata1013@gmail.com>
2025-03-06 11:19:49 +0900

    feat(neovim): Close quickfix window with `q`

--- programs/neovim/config.d/lua/config/quickfix.lua
+++ programs/neovim/config.d/lua/config/quickfix.lua
@@ -1,7 +1,15 @@
 vim.keymap.set('n', ']q', '<Cmd>cnext<CR>')
 vim.keymap.set('n', '[q', '<Cmd>cprevious<CR>')
+local augroup = vim.api.nvim_create_augroup('config-quickfix', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  group = augroup,
+  callback = function(ctx)
+    vim.keymap.set('n', 'q', vim.cmd.cclose, { buffer = ctx.buf })
+  end,
+})
 vim.api.nvim_create_autocmd('QuickFixCmdPost', {
-  group = vim.api.nvim_create_augroup('config-quickfix', { clear = true }),
+  group = augroup,
   callback = function()
     vim.cmd.cclose()
     vim.cmd.copen { mods = { split = 'belowright' } }
