856110b415c05e6138d394098ec1946420b0b8a1
Kento Ogata <k.ogata1013@gmail.com>
2025-03-06 10:20:10 +0900

    feat(neovim): Replace snacks.explorer with fern

--- programs/neovim/config.d/lua/config/plugins/fern.lua
+++ programs/neovim/config.d/lua/config/plugins/fern.lua
@@ -1,18 +1,26 @@
-local fern = {
+return {
   'lambdalisue/fern.vim',
   cmd = 'Fern',
   dependencies = {
     'lambdalisue/vim-fern-git-status',
+    'lambdalisue/vim-fern-renderer-nerdfont',
+    'lambdalisue/vim-nerdfont',
+    { 'lambdalisue/vim-fern-hijack', lazy = false },
   },
+  init = function()
+    vim.keymap.set('n', '<Plug>(filer:drawer)', '<Cmd>Fern . -drawer -reveal=%<CR>')
+  end,
   config = function()
     vim.g['fern#hide_cursor'] = 1
     vim.g['fern#default_hidden'] = 1
+    vim.g['fern#renderer'] = 'nerdfont'
     vim.fn['fern_git_status#init']()
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('config-fern', { clear = true }),
+      pattern = 'fern',
+      callback = function(ctx)
+        vim.keymap.set('n', 'q', '<Cmd>close<CR>', { buffer = ctx.buf })
+      end,
+    })
   end,
 }
-local fern_hijack = {
-  'lambdalisue/vim-fern-hijack',
-  enabled = false,
-}
-
-return { fern, fern_hijack }
--- programs/neovim/config.d/lua/config/plugins/snacks.lua
+++ programs/neovim/config.d/lua/config/plugins/snacks.lua
@@ -3,9 +3,6 @@ return {
   ---@type snacks.Config
   opts = {
     input = {},
-    explorer = {
-      replace_netrw = true,
-    },
     picker = {
       ui_select = true,
       sources = {
@@ -28,12 +25,9 @@ return {
     '<Plug>(ff)',
     '<Plug>(lsp-references)',
     '<Plug>(lsp-implementations)',
-    '<Space>e',
   },
   config = function(_, opts)
     Snacks.setup(opts)
-    -- explorer
-    vim.keymap.set('n', '<Space>e', Snacks.picker.explorer)
     -- picker
     vim.keymap.set('n', '<Plug>(ff)f', Snacks.picker.files)
     vim.keymap.set('n', '<Plug>(ff)b', Snacks.picker.buffers)
