4f60b0adea01b156d8f9735e070aa07420db29fd
Kento Ogata <k.ogata1013@gmail.com>
2025-04-09 10:25:12 +0900

    refactor: Split fzf-lua config file

--- programs/neovim/config.d/lua/config/plugins/fzf.lua
+++ programs/neovim/config.d/lua/config/plugins/fzf.lua
@@ -1,81 +1,3 @@
-local os_cmd = require 'config.os_cmd'
-
---- buffer, mrw, filesを結合して表示する
-local function smart_open()
-  local fzf = require 'fzf-lua'
-  local cwd = vim.fn.getcwd()
-  local opts = {
-    file_icons = true,
-    color_icons = true,
-    git_icons = true,
-    cwd = cwd,
-  }
-  -- git statusの取得やiconのロードなど事前に済ませておく
-  -- みたいな感じになってるぽい
-  vim.schedule(function()
-    fzf.make_entry.preprocess(opts)
-  end)
-  local function make_entry(path)
-    return fzf.make_entry.file(path, opts)
-  end
-  fzf.fzf_exec(function(next)
-    -- 同じ候補を2個表示するようなことはしない
-    local seen = {}
-    local function push_if_not_seen(path, resume)
-      path = vim.fn.fnamemodify(path, ':.')
-      if not seen[path] then
-        seen[path] = true
-        next(make_entry(path), resume)
-        return true
-      end
-    end
-
-    -- 1. 現在のbuflistを取得して表示
-    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-      if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_get_option_value('buflisted', { buf = bufnr }) then
-        local path = vim.api.nvim_buf_get_name(bufnr)
-        if path ~= '' and vim.fn.filereadable(path) == 1 and string.find(path, cwd, 1, true) then
-          push_if_not_seen(path)
-        end
-      end
-    end
-
-    -- 2. MRWリストから取得して表示
-    for _, path in ipairs(vim.fn['mr#filter'](vim.fn['mr#mrw#list'](), cwd)) do
-      if not string.match(path, '.git/COMMIT_EDITMSG') then
-        push_if_not_seen(path)
-      end
-    end
-
-    -- 3. fdで取得して表示
-    coroutine.wrap(function()
-      local co = coroutine.running()
-      for path in os_cmd.iter({ 'fd', '.', cwd }, { cwd = cwd }) do
-        local added = push_if_not_seen(path, function()
-          if coroutine.status(co) == 'suspended' then
-            coroutine.resume(co)
-          end
-        end)
-        if added then
-          coroutine.yield()
-        end
-      end
-      next()
-    end)()
-  end, {
-    prompt = 'files > ',
-    actions = {
-      ['enter'] = fzf.actions.file_edit_or_qf,
-      ['ctrl-x'] = fzf.actions.file_split,
-      ['ctrl-v'] = fzf.actions.file_vsplit,
-    },
-    previewer = 'builtin',
-    fzf_opts = {
-      ['--multi'] = true,
-    },
-  })
-end
-
 return {
   'ibhagwan/fzf-lua',
   dependencies = {
@@ -110,7 +32,7 @@ return {
   config = function(_, opts)
     require('fzf-lua').setup(opts)
 
-    vim.keymap.set('n', '<Plug>(ff)f', smart_open)
+    vim.keymap.set('n', '<Plug>(ff)f', require 'config.plugins.fzf.sources.smart-open')
     vim.keymap.set('n', '<Plug>(ff)b', '<Cmd>FzfLua buffers<CR>')
     vim.keymap.set('n', '<Plug>(ff)h', '<Cmd>FzfLua helptags<CR>')
     vim.keymap.set('n', '<Plug>(ff)/', '<Cmd>FzfLua lines<CR>')
