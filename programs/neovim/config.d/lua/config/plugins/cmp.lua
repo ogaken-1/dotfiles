6c2bab2ab234830e9249df79b5a599cce39d9458
Kento Ogata <k.ogata1013@gmail.com>
2025-04-21 19:54:10 +0900

    feat(nvim): Take word completion candidates from non-current buffers

--- programs/neovim/config.d/lua/config/plugins/cmp.lua
+++ programs/neovim/config.d/lua/config/plugins/cmp.lua
@@ -1,3 +1,11 @@
+local function get_visible_buffers()
+  local bufs = {}
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    bufs[vim.api.nvim_win_get_buf(win)] = true
+  end
+  return vim.tbl_keys(bufs)
+end
+
 local function setup_normal_sources()
   local cmp = require 'cmp'
   cmp.setup {
@@ -5,7 +13,10 @@ local function setup_normal_sources()
       { name = 'denippet' },
       { name = 'nvim_lsp' },
     }, {
-      { name = 'buffer' },
+      {
+        name = 'buffer',
+        option = { get_bufnrs = get_visible_buffers },
+      },
     }),
   }
 end
