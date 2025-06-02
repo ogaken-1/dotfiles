c143fd9003e9e66289f54838901a75e3a851424d
Kento Ogata <k.ogata1013@gmail.com>
2025-06-02 20:08:13 +0900

    feat(neovim): Popup diagnostic messages on jump

--- programs/neovim/config.d/lua/config/lsp.lua
+++ programs/neovim/config.d/lua/config/lsp.lua
@@ -1,3 +1,21 @@
+local function jump_and_show_diagnostic(count)
+  return function()
+    vim.diagnostic.jump {
+      count = count,
+      on_jump = function(diagnostic)
+        vim.diagnostic.open_float {
+          bufnr = diagnostic.bufnr,
+          lnum = diagnostic.lnum,
+          col = diagnostic.col,
+          severity = diagnostic.severity,
+          source = diagnostic.source,
+          namespace = diagnostic.namespace,
+        }
+      end,
+    }
+  end
+end
+
 local gid = vim.api.nvim_create_augroup('config-lsp', { clear = false })
 vim.api.nvim_create_autocmd('LspAttach', {
   group = gid,
@@ -13,8 +31,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gr', '<Plug>(lsp-references)', opts)
     vim.keymap.set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
     vim.keymap.set('n', 'gi', '<Plug>(lsp-implementations)', opts)
-    vim.keymap.set('n', ']d', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-    vim.keymap.set('n', '[d', '<Cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    vim.keymap.set('n', ']d', jump_and_show_diagnostic(1), opts)
+    vim.keymap.set('n', '[d', jump_and_show_diagnostic(-1), opts)
     vim.keymap.set('n', 'K', function()
       vim.lsp.buf.hover {
         border = 'rounded',
