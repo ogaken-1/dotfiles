bd0fbf50b903c340ae7ef24701dca7597c46358d
Kento Ogata <k.ogata1013@gmail.com>
2025-09-17 10:45:45 +0900

    feat(tmux): Disable mouse

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -3,7 +3,9 @@
   programs.tmux = {
     enable = true;
     escapeTime = 1;
-    mouse = true;
+    # 端末で右クリックコピーとかペーストとかしたいときに
+    # tmuxがマウスハンドルするのは不便なのでmouseは無効化する
+    mouse = false;
     prefix = "C-q";
     terminal = "tmux-256color";
     shell = "${pkgs.fish}/bin/fish";
