e94e6476eebfa4a3531e05689bccb2141b8111f9
Kento Ogata <k.ogata1013@gmail.com>
2025-10-31 09:14:40 +0900

    feat(insx): Razorの閉じタグ補完を追加

---
+++ programs/neovim/config.d/lua/config/plugins/insx/ft/razor.lua
@@ -0,0 +1,47 @@
+return {
+  add = function()
+    local insx = require 'insx'
+    local substitute = require 'insx.recipe.substitute'
+    insx.add(
+      '>',
+      insx.with(
+        substitute {
+          pattern = [[<\(\w\+\)[^>]*\%#]],
+          replace = [[\0>\%#</\1>]],
+        },
+        {
+          insx.with.filetype 'razor',
+          insx.with.in_comment(false),
+          insx.with.in_string(false),
+        }
+      )
+    )
+    insx.add(
+      '/',
+      insx.with({
+        action = function(ctx)
+          ctx.send '/>'
+        end,
+      }, {
+        insx.with.filetype 'razor',
+        insx.with.in_comment(false),
+        insx.with.in_string(false),
+        insx.with.match [[<\w\+[^>]*\%#]],
+      })
+    )
+    insx.add(
+      '<BS>',
+      insx.with(
+        substitute {
+          pattern = [[<\(\w\+\).\{-}>\%#</.\{-}>]],
+          replace = [[\%#]],
+        },
+        {
+          insx.with.filetype 'razor',
+          insx.with.in_comment(false),
+          insx.with.in_string(false),
+        }
+      )
+    )
+  end,
+}
