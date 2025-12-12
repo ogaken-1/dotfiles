753d68c8f24af075371c4dad988f4e6b18d23c5c
Kento Ogata <k.ogata1013@gmail.com>
2025-12-12 14:43:37 +0900

    feat(neovim): Window swapにもsnipewinを使う

--- programs/neovim/config.d/lua/config/plugins/snipewin.lua
+++ programs/neovim/config.d/lua/config/plugins/snipewin.lua
@@ -1,18 +1,24 @@
+local function invoke_snipewin_if_wins_morethan_3(key, action)
+  return function()
+    local focusable_wins = vim
+      .iter(vim.api.nvim_tabpage_list_wins(0))
+      :filter(function(winid)
+        return vim.api.nvim_win_get_config(winid).focusable
+      end)
+      :totable()
+    if #focusable_wins <= 2 then
+      vim.cmd.normal { bang = true, key }
+      return
+    end
+    local snipewin = require 'snipewin'
+    snipewin.select(snipewin.callback[action])
+  end
+end
 return {
   '4513ECHO/vim-snipewin',
-  keys = { '<Plug>(snipewin)' },
+  lazy = true,
   init = function()
-    vim.keymap.set('n', '<C-w><C-w>', function()
-      local focusable_wins = vim
-        .iter(vim.api.nvim_tabpage_list_wins(0))
-        :filter(function(winid)
-          return vim.api.nvim_win_get_config(winid).focusable
-        end)
-        :totable()
-      if #focusable_wins <= 2 then
-        return '<C-w><C-w>'
-      end
-      return '<Plug>(snipewin)'
-    end, { expr = true })
+    vim.keymap.set('n', '<C-w><C-w>', invoke_snipewin_if_wins_morethan_3('<C-w><C-w>', 'goto'))
+    vim.keymap.set('n', '<C-w>r', invoke_snipewin_if_wins_morethan_3('<C-w><C-w>', 'swap'))
   end,
 }
