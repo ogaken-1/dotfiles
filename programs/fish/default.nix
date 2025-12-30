2c65bf7b8430a20d65460646009b2926c6ec5660
Kento Ogata <k.ogata1013@gmail.com>
2025-12-30 17:15:01 +0900

    feat(fish): Zeno

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -8,29 +8,15 @@ in
       source = ./functions;
       recursive = true;
     };
+    "${config.xdg.configHome}/zeno/config.yaml" = {
+      source = ./zeno.yaml;
+    };
   };
   home.packages = [
     zeno
   ];
   programs.fish = {
     enable = true;
-    shellAbbrs = {
-      ga = "git add";
-      gci = "git commit";
-      gcia = "git commit --amend";
-      gcif = "git commit --fixup=";
-      gd = "git diff";
-      gf = "git fetch";
-      gF = "git pull";
-      gp = "git push";
-      gpf = "git push --force-if-includes --force-with-lease";
-      gpu = "git push --set-upstream origin \"$(git branch --show-current)\"";
-      gs = "git status --short --branch -uall";
-      gst = "git stash";
-      gstl = "git stash list";
-      gsw = "git switch";
-      gswc = "git switch --create";
-    };
     shellInit = ''
       if status --is-interactive && test -z $TMUX
         bind \cqs 'tmux attach-session -t (tmux list-sessions -F \'#{session_name}\' 2> /dev/null | fzf --layout=reverse --border=block) > /dev/null 2> /dev/null'
