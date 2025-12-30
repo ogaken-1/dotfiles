dafb8c531ca9a75d0b36a4b082e9fe83adf10bd1
Kento Ogata <k.ogata1013@gmail.com>
2025-12-30 17:15:40 +0900

    refactor(fish): インラインワンライナー → 関数

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -17,9 +17,19 @@ in
   ];
   programs.fish = {
     enable = true;
+    functions = {
+      fzf_tmux_session = {
+        body = ''
+          tmux attach-session -t (
+            tmux list-sessions -F '#{session_name}' 2> /dev/null |
+              fzf --layout=reverse --border=block
+          ) > /dev/null 2> /dev/null
+        '';
+      };
+    };
     shellInit = ''
-      if status --is-interactive && test -z $TMUX
-        bind \cqs 'tmux attach-session -t (tmux list-sessions -F \'#{session_name}\' 2> /dev/null | fzf --layout=reverse --border=block) > /dev/null 2> /dev/null'
+      if status --is-interactive && [ -z $TMUX ]
+        bind \cqs fzf_tmux_session
       end
       if [ "''$ZENO_LOADED" = "1" ]
         bind ' ' zeno-auto-snippet
