ac510c8815708fd3641be8cd5e33b0778ce39969
Kento Ogata <k.ogata1013@gmail.com>
2026-01-03 15:31:47 +0900

    feat(neovim): Aerialでprismaファイルのoutlineを表示する

---
+++ programs/neovim/config.d/queries/prisma/aerial.scm
@@ -0,0 +1,11 @@
+(generator_declaration
+  (identifier) @name) @symbol
+
+(datasource_declaration
+  (identifier) @name) @symbol
+
+(model_declaration
+  (identifier) @name) @symbol
+
+(column_declaration
+  (identifier) @name) @symbol
